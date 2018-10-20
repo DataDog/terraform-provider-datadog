014346cef9d129fb3bac89aa2e1b348ce50138e1
Ryuzo Yamamoto <ryuzo.yamamoto@gmail.com>
2018-10-20 18:55:32 +0900

    Add "datadog_integration_gcp" resource for Google Cloud Platform integration

---
+++ datadog/resource_datadog_integration_gcp_test.go
@@ -0,0 +1,123 @@
+package datadog
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	datadog "github.com/zorkian/go-datadog-api"
+)
+
+const testAccCheckDatadogIntegrationGCPConfig = `
+resource "datadog_integration_gcp" "awesome_gcp_project_integration" {
+  project_id     = "awesome-project-id"
+  private_key_id = "1234567890123456789012345678901234567890"
+  private_key    = "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n"
+  client_email   = "awesome-service-account@awesome-project-id.iam.gserviceaccount.com"
+  client_id      = "123456789012345678901"
+  host_filters   = "foo:bar,buzz:lightyear"
+}
+`
+const testAccCheckDatadogIntegrationGCPEmptyHostFiltersConfig = `
+resource "datadog_integration_gcp" "awesome_gcp_project_integration" {
+  project_id     = "awesome-project-id"
+  private_key_id = "1234567890123456789012345678901234567890"
+  private_key    = "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n"
+  client_email   = "awesome-service-account@awesome-project-id.iam.gserviceaccount.com"
+  client_id      = "123456789012345678901"
+}
+`
+
+func TestAccDatadogIntegrationGCP(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: checkIntegrationGCPDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckDatadogIntegrationGCPConfig,
+				Check: resource.ComposeTestCheckFunc(
+					checkIntegrationGCPExists,
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"project_id", "awesome-project-id"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"private_key_id", "1234567890123456789012345678901234567890"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"private_key", "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"client_email", "awesome-service-account@awesome-project-id.iam.gserviceaccount.com"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"client_id", "123456789012345678901"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"host_filters", "foo:bar,buzz:lightyear"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccCheckDatadogIntegrationGCPEmptyHostFiltersConfig,
+				Check: resource.ComposeTestCheckFunc(
+					checkIntegrationGCPExists,
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"project_id", "awesome-project-id"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"private_key_id", "1234567890123456789012345678901234567890"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"private_key", "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"client_email", "awesome-service-account@awesome-project-id.iam.gserviceaccount.com"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"client_id", "123456789012345678901"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_gcp.awesome_gcp_project_integration",
+						"host_filters", ""),
+				),
+			},
+		},
+	})
+}
+
+func checkIntegrationGCPExists(s *terraform.State) error {
+	client := testAccProvider.Meta().(*datadog.Client)
+	integrations, err := client.ListIntegrationGCP()
+	if err != nil {
+		return err
+	}
+	for _, r := range s.RootModule().Resources {
+		projectID := r.Primary.ID
+		for _, integration := range integrations {
+			if integration.GetProjectID() == projectID {
+				return nil
+			}
+		}
+		return fmt.Errorf("The Google Cloud Platform integration doesn't exist: projectID=%s", projectID)
+	}
+	return nil
+}
+
+func checkIntegrationGCPDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*datadog.Client)
+	integrations, err := client.ListIntegrationGCP()
+	if err != nil {
+		return err
+	}
+	for _, r := range s.RootModule().Resources {
+		projectID := r.Primary.ID
+		for _, integration := range integrations {
+			if integration.GetProjectID() == projectID {
+				return fmt.Errorf("The Google Cloud Platform integration still exist: projectID=%s", projectID)
+			}
+		}
+	}
+	return nil
+}
