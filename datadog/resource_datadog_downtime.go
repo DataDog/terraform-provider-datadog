5c450772770c91dbe15aa649ba61f7c252a747fc
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-01-15 20:03:22 -0500

    Same for end_date

--- datadog/resource_datadog_downtime.go
+++ datadog/resource_datadog_downtime.go
@@ -129,13 +129,14 @@ func buildDowntimeStruct(d *schema.ResourceData) *datadog.Downtime {
 	if attr, ok := d.GetOk("disabled"); ok {
 		dt.SetDisabled(attr.(bool))
 	}
-	if attr, ok := d.GetOk("end"); ok {
-		dt.SetEnd(attr.(int))
-	} else if attr, ok := d.GetOk("end_date"); ok {
+	if attr, ok := d.GetOk("end_date"); ok {
 		if t, err := time.Parse(time.RFC3339, attr.(string)); err == nil {
 			dt.SetEnd(int(t.Unix()))
 		}
+	} else if attr, ok := d.GetOk("end"); ok {
+		dt.SetEnd(attr.(int))
 	}
+
 	if attr, ok := d.GetOk("message"); ok {
 		dt.SetMessage(strings.TrimSpace(attr.(string)))
 	}
