dd7a34993cc2329c5b8c55bfbbebfa6dcc08678b
unclebconnor <brian.connor@datadoghq.com>
2019-11-14 11:33:20 -0500

    required true instead of optional false

--- datadog/resource_datadog_service_level_objective.go
+++ datadog/resource_datadog_service_level_objective.go
@@ -112,7 +112,7 @@ func resourceDatadogServiceLevelObjective() *schema.Resource {
 			// Monitor-Based SLO
 			"monitor_ids": {
 				Type:          schema.TypeSet,
-				Optional:      false,
+				Required:      true,
 				ConflictsWith: []string{"query"},
 				Description:   "A static set of monitor IDs to use as part of the SLO",
 				Elem:          &schema.Schema{Type: schema.TypeInt, MinItems: 1},
