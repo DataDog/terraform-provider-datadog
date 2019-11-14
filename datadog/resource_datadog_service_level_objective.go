8a416f2d50960f5ba3dbd899748625dcecf4135f
unclebconnor <brian.connor@datadoghq.com>
2019-11-14 11:39:03 -0500

    cant use required and conflictswith

--- datadog/resource_datadog_service_level_objective.go
+++ datadog/resource_datadog_service_level_objective.go
@@ -112,7 +112,7 @@ func resourceDatadogServiceLevelObjective() *schema.Resource {
 			// Monitor-Based SLO
 			"monitor_ids": {
 				Type:          schema.TypeSet,
-				Required:      true,
+				Optional:      true,
 				ConflictsWith: []string{"query"},
 				Description:   "A static set of monitor IDs to use as part of the SLO",
 				Elem:          &schema.Schema{Type: schema.TypeInt, MinItems: 1},
