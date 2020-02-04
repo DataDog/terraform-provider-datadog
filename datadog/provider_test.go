fcfb51f384cfa1729ae26183f6b5ba49296707fc
Radek Simko <radek.simko@gmail.com>
2020-02-04 15:48:30 +0100

    opt into binary driver

--- datadog/provider_test.go
+++ datadog/provider_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -16,6 +18,11 @@ func init() {
 	testAccProviders = map[string]terraform.ResourceProvider{
 		"datadog": testAccProvider,
 	}
+}
+
+func TestMain(m *testing.M) {
+	acctest.UseBinaryDriver("datadog", Provider)
+	resource.TestMain(m)
 }
 
 func TestProvider(t *testing.T) {
@@ -34,5 +41,10 @@ func testAccPreCheck(t *testing.T) {
 	}
 	if v := os.Getenv("DATADOG_APP_KEY"); v == "" {
 		t.Fatal("DATADOG_APP_KEY must be set for acceptance tests")
+	}
+
+	err := testAccProvider.Configure(terraform.NewResourceConfigRaw(nil))
+	if err != nil {
+		t.Fatal(err)
 	}
 }
