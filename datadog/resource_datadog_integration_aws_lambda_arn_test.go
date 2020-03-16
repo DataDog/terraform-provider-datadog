da476fe7c557c19185f65f78e68222a65e8a25c5
Mark Haley <mhaley@miovision.com>
2020-03-16 10:03:17 -0400

    Add acceptance test for aws log collection lambda arn.

--- datadog/resource_datadog_integration_aws_lambda_arn_test.go
+++ datadog/resource_datadog_integration_aws_lambda_arn_test.go
@@ -2,6 +2,9 @@ package datadog
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/zorkian/go-datadog-api"
 	"testing"
 )
 
@@ -34,4 +37,80 @@ func TestAccountAndLambdaArnFromID(t *testing.T) {
 			t.Errorf("%s: lambda arn '%s' didn't match `%s`", name, lambdaArn, tc.lambdaArn)
 		}
 	}
+}
+
+const testAccDatadogIntegrationAWSLambdaArnConfig = `
+resource "datadog_integration_aws" "account" {
+  account_id                       = "1234567890"
+  role_name                        = "testacc-datadog-integration-role"
+}
+
+resource "datadog_integration_aws_lambda_arn" "main_collector" {
+  account_id = "1234567890"
+  lambda_arn = "arn:aws:lambda:us-east-1:1234567890:function:datadog-forwarder-Forwarder"
+  depends_on = [datadog_integration_aws.account]
+}
+`
+
+func TestAccDatadogIntegrationAWSLambdaArn(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: checkIntegrationAWSLambdaArnDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatadogIntegrationAWSLambdaArnConfig,
+				Check: resource.ComposeTestCheckFunc(
+					checkIntegrationAWSLambdaArnExists,
+					resource.TestCheckResourceAttr(
+						"datadog_integration_aws_lambda_arn.main_collector",
+						"account_id", "1234567890"),
+					resource.TestCheckResourceAttr(
+						"datadog_integration_aws_lambda_arn.main_collector",
+						"lambda_arn", "arn:aws:lambda:us-east-1:1234567890:function:datadog-forwarder-Forwarder"),
+				),
+			},
+		},
+	})
+}
+
+func checkIntegrationAWSLambdaArnExists(s *terraform.State) error {
+	client := testAccProvider.Meta().(*datadog.Client)
+	logCollections, err := client.GetIntegrationAWSLogCollection()
+	if err != nil {
+		return err
+	}
+	for _, r := range s.RootModule().Resources {
+		accountId := r.Primary.Attributes["account_id"]
+		lambdaArn := r.Primary.Attributes["lambda_arn"]
+		for _, logCollection := range *logCollections {
+			for _, logCollectionLambdaArn := range logCollection.LambdaARNs {
+				if *logCollection.AccountID == accountId && *logCollectionLambdaArn.LambdaARN == lambdaArn {
+					return nil
+				}
+			}
+		}
+		return fmt.Errorf("The AWS Lambda ARN is not attached to the account: accountId=%s, lambdaArn=%s", accountId, lambdaArn)
+	}
+	return nil
+}
+
+func checkIntegrationAWSLambdaArnDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*datadog.Client)
+	logCollections, err := client.GetIntegrationAWSLogCollection()
+	if err != nil {
+		return err
+	}
+	for _, r := range s.RootModule().Resources {
+		accountId := r.Primary.Attributes["account_id"]
+		lambdaArn := r.Primary.Attributes["lambda_arn"]
+		for _, logCollection := range *logCollections {
+			for _, logCollectionLambdaArn := range logCollection.LambdaARNs {
+				if *logCollection.AccountID == accountId && *logCollectionLambdaArn.LambdaARN == lambdaArn {
+					return fmt.Errorf("The AWS Lambda ARN is still attached to the account: accountId=%s, lambdaArn=%s", accountId, lambdaArn)
+				}
+			}
+		}
+	}
+	return nil
 }
