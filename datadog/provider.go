23f228dac58e31fb94bf8b7a896242137048c0b7
skarimo <sherzod.karimov@datadoghq.com>
2020-04-29 22:52:57 -0400

    revert provider.go change

--- datadog/provider.go
+++ datadog/provider.go
@@ -142,7 +142,6 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 			"protocol": parsedApiUrl.Scheme,
 		})
 	}
-	configV1.Debug = true
 	datadogClientV1 := datadogV1.NewAPIClient(configV1)
 
 	// Initialize the official Datadog V2 API client
@@ -174,7 +173,6 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 			"protocol": parsedApiUrl.Scheme,
 		})
 	}
-	configV2.Debug = true
 	datadogClientV2 := datadogV2.NewAPIClient(configV2)
 
 	return &ProviderConfiguration{
