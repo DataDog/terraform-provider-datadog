275c62dd070f899652190a1da208431b3c252735
Thomas Hervé <thomas.herve@datadoghq.com>
2020-07-27 10:37:55 +0200

    Additionally check for start in the past

--- datadog/resource_datadog_downtime.go
+++ datadog/resource_datadog_downtime.go
@@ -46,8 +46,9 @@ func resourceDatadogDowntime() *schema.Resource {
 				Optional: true,
 				DiffSuppressFunc: func(k, oldVal, newVal string, d *schema.ResourceData) bool {
 					_, startDatePresent := d.GetOk("start_date")
-					// If "start_date" is set, ignore diff for "start". If "start" isn't set, ignore diff because the value was set automatically.
-					return startDatePresent || (newVal == "0" && oldVal != "0")
+					now := time.Now().Unix()
+					// If "start_date" is set, ignore diff for "start". If "start" isn't set, ignore diff if start is in the past
+					return startDatePresent || (newVal == "0" && oldVal != "0" && int64(d.Get("start").(int)) < now)
 				},
 				Description: "Specify when this downtime should start",
 			},
