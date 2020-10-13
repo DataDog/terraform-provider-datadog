f7847bb4e38f669e92dc9681d0a0d894b05fcc79
Vincent Roy <vincent.roy@datadoghq.com>
2020-10-13 14:39:25 +0200

    create buildDatadogArchiveOrderCreateReq function

--- datadog/resource_datadog_logs_archive_order.go
+++ datadog/resource_datadog_logs_archive_order.go
@@ -48,17 +48,15 @@ func resourceDatadogLogsArchiveOrderRead(d *schema.ResourceData, meta interface{
 }
 
 func resourceDatadogLogsArchiveOrderUpdate(d *schema.ResourceData, meta interface{}) error {
-	var ddArchiveList datadogV2.LogsArchiveOrder
-	tfList := d.Get("archive_ids").([]interface{})
-	ddList := make([]string, len(tfList))
-	for i, id := range tfList {
-		ddList[i] = id.(string)
+	ddArchiveList, err := buildDatadogArchiveOrderCreateReq(d)
+	if err != nil {
+		return err
 	}
-	ddArchiveList.Data.Attributes.ArchiveIds = ddList
+
 	providerConf := meta.(*ProviderConfiguration)
 	datadogClientV2 := providerConf.DatadogClientV2
 	authV2 := providerConf.AuthV2
-	if _, _, err := datadogClientV2.LogsArchivesApi.UpdateLogsArchiveOrder(authV2).Body(ddArchiveList).Execute(); err != nil {
+	if _, _, err := datadogClientV2.LogsArchivesApi.UpdateLogsArchiveOrder(authV2).Body(*ddArchiveList).Execute(); err != nil {
 		// Cannot map archives to existing ones
 		if strings.Contains(err.Error(), "422 Unprocessable Entity") {
 			ddArchiveOrder, _, getErr := datadogClientV2.LogsArchivesApi.GetLogsArchiveOrder(authV2).Execute()
@@ -67,7 +65,7 @@ func resourceDatadogLogsArchiveOrderUpdate(d *schema.ResourceData, meta interfac
 			}
 			return fmt.Errorf("cannot map archives to existing ones\n existing archives: %s\n archive to be updated: %s",
 				ddArchiveOrder.Data.Attributes.ArchiveIds,
-				ddList)
+				ddArchiveList.Data.Attributes.GetArchiveIds())
 		}
 		return translateClientError(err, "error updating logs archive order")
 	}
@@ -82,4 +80,25 @@ func resourceDatadogLogsArchiveOrderDelete(_ *schema.ResourceData, _ interface{}
 
 func resourceDatadogLogsArchiveOrderExists(_ *schema.ResourceData, _ interface{}) (bool, error) {
 	return true, nil
+}
+
+func getArchiveIds(d *schema.ResourceData) []string {
+	tfList := d.Get("archive_ids").([]interface{})
+	ddList := make([]string, len(tfList))
+	for i, id := range tfList {
+		ddList[i] = id.(string)
+	}
+	return ddList
+}
+
+//Map to model
+func buildDatadogArchiveOrderCreateReq(d *schema.ResourceData) (*datadogV2.LogsArchiveOrder, error) {
+	archiveOrderAttributes := datadogV2.NewLogsArchiveOrderAttributes(getArchiveIds(d))
+
+	archiveOrderDefinition := datadogV2.NewLogsArchiveOrderDefinitionWithDefaults()
+	archiveOrderDefinition.SetAttributes(*archiveOrderAttributes)
+
+	archiveOrder := datadogV2.NewLogsArchiveOrderWithDefaults()
+	archiveOrder.SetData(*archiveOrderDefinition)
+	return archiveOrder, nil
 }
