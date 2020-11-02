7c2c97357cefc214fd1fd9b2f50ada053308d7cc
Thomas Hervé <thomas.herve@datadoghq.com>
2020-11-02 17:42:01 +0100

    Update to released go client (#738)
    Prepare for release

--- vendor/github.com/DataDog/datadog-api-client-go/api/v1/datadog/api_events.go
+++ vendor/github.com/DataDog/datadog-api-client-go/api/v1/datadog/api_events.go
@@ -226,7 +226,13 @@ func (r ApiListEventsRequest) Execute() (EventListResponse, *_nethttp.Response, 
  * ListEvents Query the event stream
  * The event stream can be queried and filtered by time, priority, sources and tags.
 
-**Note**: If the event you’re querying contains markdown formatting of any kind, you may see characters such as %,\,n in your output.
+**Notes**:
+- If the event you’re querying contains markdown formatting of any kind,
+you may see characters such as `%`,`\`,`n` in your output.
+
+- This endpoint returns a maximum of `1000` most recent results. To return additional results,
+identify the last timestamp of the last result and set that as the `end` query time to
+paginate the results.
  * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @return ApiListEventsRequest
 */
