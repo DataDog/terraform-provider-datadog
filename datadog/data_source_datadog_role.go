67f06c400d155abc12b34568cb1544a83f565cd9
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2020-11-13 16:28:17 +0100

    Deal with exact match

--- datadog/data_source_datadog_role.go
+++ datadog/data_source_datadog_role.go
@@ -2,6 +2,7 @@ package datadog
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -13,7 +14,7 @@ func dataSourceDatadogRole() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"filter": {
 				Type:     schema.TypeString,
-				Optional: true,
+				Optional: false,
 			},
 
 			// Computed values
@@ -35,23 +36,37 @@ func dataSourceDatadogRoleRead(d *schema.ResourceData, meta interface{}) error {
 	authV2 := providerConf.AuthV2
 
 	req := datadogClientV2.RolesApi.ListRoles(authV2)
-	if v, ok := d.GetOk("filter"); ok {
-		req = req.Filter(v.(string))
-	}
+	filter := d.Get("filter").(string)
+	req = req.Filter(filter)
 
 	res, _, err := req.Execute()
 	if err != nil {
 		return translateClientError(err, "error querying roles")
 	}
 	roles := res.GetData()
+	roleIndex := 0
 	if len(roles) > 1 {
-		return fmt.Errorf("your query returned more than one result, please try a more specific search criteria")
-	}
-	if len(roles) == 0 {
+		exactMatchFound := false
+		for i, role := range roles {
+			if role.Attributes.GetName() == filter {
+				exactMatchFound = true
+				roleIndex = i
+				log.Printf("[INFO] Got multiple matches for role %s, picking the one with this exact name", filter)
+				break
+			}
+		}
+		if !exactMatchFound {
+			return fmt.Errorf(
+				"your query returned more than one result and no exact match for name '%s' were found, "+
+					"please try a more specific search criteria",
+				filter,
+			)
+		}
+	} else if len(roles) == 0 {
 		return fmt.Errorf("your query returned no result, please try a less specific search criteria")
 	}
 
-	r := roles[0]
+	r := roles[roleIndex]
 	d.SetId(r.GetId())
 	d.Set("name", r.Attributes.GetName())
 	d.Set("user_count", r.Attributes.GetUserCount())
