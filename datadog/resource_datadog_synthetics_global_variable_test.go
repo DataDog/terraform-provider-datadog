d75986971ddf885277a1aeceacf7e9ab46b2af02
Romain Berger <romain.berger@datadoghq.com>
2020-11-23 12:15:51 +0100

    [Synthetics] Add support for secure global variables

--- datadog/resource_datadog_synthetics_global_variable_test.go
+++ datadog/resource_datadog_synthetics_global_variable_test.go
@@ -53,6 +53,21 @@ func TestAccDatadogSyntheticsGlobalVariable_Basic(t *testing.T) {
 	})
 }
 
+func TestAccDatadogSyntheticsGlobalVariableSecure_Basic(t *testing.T) {
+	accProviders, clock, cleanup := testAccProviders(t, initRecorder(t))
+	defer cleanup(t)
+	accProvider := testAccProvider(t, accProviders)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    accProviders,
+		CheckDestroy: testSyntheticsGlobalVariableIsDestroyed(accProvider),
+		Steps: []resource.TestStep{
+			createSyntheticsGlobalVariableSecureStep(accProvider, clock, t),
+		},
+	})
+}
+
 func TestAccDatadogSyntheticsGlobalVariable_Updated(t *testing.T) {
 	accProviders, clock, cleanup := testAccProviders(t, initRecorder(t))
 	defer cleanup(t)
@@ -65,6 +80,22 @@ func TestAccDatadogSyntheticsGlobalVariable_Updated(t *testing.T) {
 		Steps: []resource.TestStep{
 			createSyntheticsGlobalVariableStep(accProvider, clock, t),
 			updateSyntheticsGlobalVariableStep(accProvider, clock, t),
+		},
+	})
+}
+
+func TestAccDatadogSyntheticsGlobalVariableSecure_Updated(t *testing.T) {
+	accProviders, clock, cleanup := testAccProviders(t, initRecorder(t))
+	defer cleanup(t)
+	accProvider := testAccProvider(t, accProviders)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    accProviders,
+		CheckDestroy: testSyntheticsGlobalVariableIsDestroyed(accProvider),
+		Steps: []resource.TestStep{
+			createSyntheticsGlobalVariableSecureStep(accProvider, clock, t),
+			updateSyntheticsGlobalVariableSecureStep(accProvider, clock, t),
 		},
 	})
 }
@@ -132,6 +163,78 @@ resource "datadog_synthetics_global_variable" "foo" {
 	description = "an updated global variable"
 	tags = ["foo:bar", "baz", "env:test"]
 	value = "variable-value-updated"
+}`, uniq)
+}
+
+func createSyntheticsGlobalVariableSecureStep(accProvider *schema.Provider, clock clockwork.FakeClock, t *testing.T) resource.TestStep {
+	variableName := getUniqueVariableName(clock, t)
+	return resource.TestStep{
+		Config: createSyntheticsGlobalVariableSecureConfig(variableName),
+		Check: resource.ComposeTestCheckFunc(
+			testSyntheticsGlobalVariableExists(accProvider),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "name", variableName),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "description", "a secure global variable"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "tags.#", "2"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "tags.0", "foo:bar"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "tags.1", "baz"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "value", "variable-secure-value"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "secure", "true"),
+		),
+	}
+}
+
+func createSyntheticsGlobalVariableSecureConfig(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_synthetics_global_variable" "foo" {
+	name = "%s"
+	description = "a secure global variable"
+	tags = ["foo:bar", "baz"]
+	value = "variable-secure-value"
+	secure = true
+}`, uniq)
+}
+
+func updateSyntheticsGlobalVariableSecureStep(accProvider *schema.Provider, clock clockwork.FakeClock, t *testing.T) resource.TestStep {
+	variableName := getUniqueVariableName(clock, t) + "_UPDATED"
+	return resource.TestStep{
+		Config: updateSyntheticsGlobalVariableSecureConfig(variableName),
+		Check: resource.ComposeTestCheckFunc(
+			testSyntheticsGlobalVariableExists(accProvider),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "name", variableName),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "description", "an updated secure global variable"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "tags.#", "3"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "tags.0", "foo:bar"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "tags.1", "baz"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "tags.2", "env:test"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "value", "variable-secure-value-updated"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_global_variable.foo", "secure", "true"),
+		),
+	}
+}
+
+func updateSyntheticsGlobalVariableSecureConfig(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_synthetics_global_variable" "foo" {
+	name = "%s"
+	description = "an updated secure global variable"
+	tags = ["foo:bar", "baz", "env:test"]
+	value = "variable-secure-value-updated"
+	secure = true
 }`, uniq)
 }
 
