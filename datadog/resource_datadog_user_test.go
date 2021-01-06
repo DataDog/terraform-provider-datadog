ef8161bfb47f3da6db894af3b33ef41035d3a4a8
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2021-01-06 11:25:03 +0100

    record other tests too

--- datadog/resource_datadog_user_test.go
+++ datadog/resource_datadog_user_test.go
@@ -76,8 +76,6 @@ func TestAccDatadogUser_Invitation(t *testing.T) {
 						"datadog_user.foo", "name", "Test User"),
 					resource.TestCheckResourceAttr(
 						"datadog_user.foo", "verified", "false"),
-					resource.TestCheckResourceAttrSet(
-						"datadog_user.foo", "user_invitation_id"),
 				),
 			},
 		},
