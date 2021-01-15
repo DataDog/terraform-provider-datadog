e973a92cfe1e7163e2c757af5199ebe630c59a25
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2021-01-15 15:49:17 -0500

    Update examples/resources/datadog_monitor/resource.tf
    Co-authored-by: Hippolyte HENRY <zippolyte@users.noreply.github.com>

--- examples/resources/datadog_monitor/resource.tf
+++ examples/resources/datadog_monitor/resource.tf
@@ -7,7 +7,7 @@ resource "datadog_monitor" "foo" {
 
   query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 4"
 
-  thresholds = {
+  monitor_thresholds {
     warning           = 2
     warning_recovery  = 1
     critical          = 4
