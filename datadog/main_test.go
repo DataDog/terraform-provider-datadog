93bdb3e943e874da7329eb9d88e6ce5794e4a0b6
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-01-28 18:17:46 +0100

    version info

--- datadog/main_test.go
+++ datadog/main_test.go
@@ -1,10 +1,12 @@
 package datadog
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/meta"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
 )
@@ -23,6 +25,10 @@ func TestMain(m *testing.M) {
 
 	err := profiler.Start(
 		profiler.WithService(service),
+		profiler.WithTags(
+			fmt.Sprintf("terraform.sdk:%s", meta.SDKVersionString()),
+			fmt.Sprintf("terraform.cli:%s", datadogProvider.TerraformVersion),
+		),
 		profiler.WithProfileTypes(profiler.BlockProfile, profiler.CPUProfile, profiler.GoroutineProfile, profiler.HeapProfile),
 	)
 	if err != nil {
