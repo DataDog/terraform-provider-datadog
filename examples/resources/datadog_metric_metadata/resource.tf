f775d5c9ffeca4b108d1d0b867eaceec05aa9a9d
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2021-02-05 10:19:03 -0500

    Generate metrics_metadata docs (#894)

---
+++ examples/resources/datadog_metric_metadata/resource.tf
@@ -0,0 +1,8 @@
+# Manage a Datadog metric's metadata
+resource "datadog_metric_metadata" "request_time" {
+  metric      = "request.time"
+  short_name  = "Request time"
+  description = "99th percentile request time in millseconds"
+  type        = "gauge"
+  unit        = "millisecond"
+}
