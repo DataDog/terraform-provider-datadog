bef6e161826a0595e37e6ca3800c555fff05c060
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2021-02-05 14:41:00 -0500

    Show older variable syntax, but comment out

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -15,33 +15,12 @@ provider "datadog" {
   app_key = var.datadog_app_key
 }
 
-# Create a new monitor
-resource "datadog_monitor" "default" {
-  # ...
-}
-
-# Create a new dashboard
-resource "datadog_dashboard" "default" {
-  # ...
-}
-
-
 
 
 # Terraform 0.12- can be specified as:
 
 # Configure the Datadog provider
-provider "datadog" {
-  api_key = "<DD_API_KEY>"
-  app_key = "<DD_APP_KEY>"
-}
-
-# Create a new monitor
-resource "datadog_monitor" "default" {
-  # ...
-}
-
-# Create a new dashboard
-resource "datadog_dashboard" "default" {
-  # ...
-}
+# provider "datadog" {
+#   api_key = "${var.datadog_api_key}"
+#   app_key = "${var.datadog_app_key}"
+# }
