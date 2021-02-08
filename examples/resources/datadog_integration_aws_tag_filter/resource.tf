8bcd4a2e4afef48033d638a44a27e26ecd180f24
skarimo <sherzod.karimov@datadoghq.com>
2021-02-08 11:26:45 -0500

    fmt

--- examples/resources/datadog_integration_aws_tag_filter/resource.tf
+++ examples/resources/datadog_integration_aws_tag_filter/resource.tf
@@ -1,5 +1,5 @@
 resource "datadog_integration_aws_tag_filter" "foo" {
-  account_id = "123456789010"
-  namespace = "sqs"
+  account_id     = "123456789010"
+  namespace      = "sqs"
   tag_filter_str = "key:value"
 }
