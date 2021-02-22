efefe5a0ed0682a4c7a37cc6cf0e279062ec9d2a
Eric Fraese <eric.fraese@datadoghq.com>
2021-02-22 11:37:34 -0500

    added first pass on example for tag config resource

---
+++ examples/resources/datadog_metric_tag_configuration/resource.tf
@@ -0,0 +1,8 @@
+# Manage a Datadog metric's metadata
+resource "datadog_metric_tag_configuration" "testing_metric_tag_config" {
+			# TODO[efraese] use an id for a distribution metric
+      id = ""
+			metric_type = "distribution"
+			tags = ["sport","datacenter"]
+			include_percentiles = true
+}
