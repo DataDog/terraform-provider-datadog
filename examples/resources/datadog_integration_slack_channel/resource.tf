5cafaaf000bc4ab126a8fa7212c1aee9aef10fb0
Sherzod K <sherzod.karimov@datadoghq.com>
2021-03-01 10:38:12 -0500

    generate slack channel docs

---
+++ examples/resources/datadog_integration_slack_channel/resource.tf
@@ -0,0 +1,10 @@
+resource "datadog_integration_slack_channel" "slack_channel" {
+  display {
+    message = true
+    notified = false
+    snapshot = false
+    tags = true
+  }
+  channel_name = "#test_channel"
+  account_name    = "foo"
+}
