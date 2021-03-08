240e1debd033e3226cb3fa9e5b93d2a25a0096bc
Sherzod K <sherzod.karimov@datadoghq.com>
2021-03-08 17:16:50 -0500

    test

--- scripts/get-terraform-binary.sh
+++ scripts/get-terraform-binary.sh
@@ -8,17 +8,17 @@ echo "OS is $(uname -s)"
 
 case "$(uname -s)" in
    MINGW64*)
+    echo "is wget command being executed"
     wget -O tf.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_windows_amd64.zip
     FILE_NAME=$(unzip -Z -1 tf.zip)
      ;;
-
    *)
     wget -O tf.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip
     FILE_NAME=$(unzip -Z -1 tf.zip)
      ;;
 esac
 
-unzip 'tf.zip'
+unzip './tf.zip'
 TF_BINARY_PATH=$(pwd)/${FILE_NAME}
 
 export TF_ACC_TERRAFORM_PATH=${TF_BINARY_PATH}
