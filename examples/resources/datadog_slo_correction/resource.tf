b892d6c2a5a13ec13e4c7063c09f55b6afa066b1
zhengshizhao <zhengshi.zhao@datadoghq.com>
2021-04-08 15:56:11 -0400

    add an exmaple for slo correction

--- examples/resources/datadog_slo_correction/resource.tf
+++ examples/resources/datadog_slo_correction/resource.tf
@@ -1,26 +1,26 @@
 # Create a new Datadog SLO Correction. slo_id can be derived from slo resource or specify an slo id for existing a SLO.  
 
-resource "datadog_service_level_objective" "foo" {
-  name = "example slo"
-  type = "metric"
-  description = "some updated description about foo SLO"
+resource "datadog_service_level_objective" "example_slo" {
+  name        = "example slo"
+  type        = "metric"
+  description = "some updated description about example_slo SLO"
   query {
-    numerator = "sum:my.metric{type:good}.as_count()"
+    numerator   = "sum:my.metric{type:good}.as_count()"
     denominator = "sum:my.metric{type:good}.as_count() + sum:my.metric{type:bad}.as_count()"
   }
 
   thresholds {
     timeframe = "7d"
-    target = 99.5
-    warning = 99.8
+    target    = 99.5
+    warning   = 99.8
   }
   tags = ["foo:bar"]
 }
-resource "datadog_slo_correction" "testing_slo_correction" {
-  category = "Scheduled Maintenance"
+resource "datadog_slo_correction" "example_slo_correction" {
+  category    = "Scheduled Maintenance"
   description = "correction example"
-  end = 1735718600
-  slo_id = "datadog_service_level_objective.foo.id"
-  start = 1735707000
-  timezone = "UTC"
+  end         = 1735718600
+  slo_id      = "datadog_service_level_objective.example_slo.id"
+  start       = 1735707000
+  timezone    = "UTC"
 }
