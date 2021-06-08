1001d391c0afea437dc278c0b934cf06cd7934d6
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2021-06-08 10:22:05 +0200

    Format example

--- examples/resources/datadog_logs_index/resource.tf
+++ examples/resources/datadog_logs_index/resource.tf
@@ -1,8 +1,8 @@
 # A sample Datadog logs index resource definition. Note that at this point, it is not possible to create new logs indexes through Terraform, so the name field must match a name of an already existing index. If you want to keep the current state of the index, we suggest importing it (see below).
 
 resource "datadog_logs_index" "sample_index" {
-  name = "your index"
-  daily_limit = 200000
+  name           = "your index"
+  daily_limit    = 200000
   retention_days = 7
   filter {
     query = "*"
