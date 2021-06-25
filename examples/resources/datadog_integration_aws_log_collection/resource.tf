32d625c0f2c4c5c6af273698c29ca62dbeb043d7
Peter Mescalchin <peter@magnetikonline.com>
2021-06-25 17:57:53 +1000

    Fixed Terraform examples/ for `datadog_integration_aws_log_collection` / `datadog_integration_aws_tag_filter` with their comments

--- examples/resources/datadog_integration_aws_log_collection/resource.tf
+++ examples/resources/datadog_integration_aws_log_collection/resource.tf
@@ -1,4 +1,4 @@
-# Create a new Datadog - Amazon Web Services integration lambda arn
+# Create a new Datadog - Amazon Web Services integration log collection
 resource "datadog_integration_aws_log_collection" "main" {
   account_id = "1234567890"
   services   = ["lambda"]
--- examples/resources/datadog_integration_aws_tag_filter/resource.tf
+++ examples/resources/datadog_integration_aws_tag_filter/resource.tf
@@ -1,3 +1,4 @@
+# Create a new Datadog - Amazon Web Services integration tag filter
 resource "datadog_integration_aws_tag_filter" "foo" {
   account_id     = "123456789010"
   namespace      = "sqs"
