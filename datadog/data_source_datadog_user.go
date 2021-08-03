1b4163a00da83cbe203e98163dd9fd7b07e7fa92
Thomas Leveque <thomas.leveque@lumapps.com>
2021-08-03 17:40:05 +0200

    removes unsued comments, variables and logic
    Co-authored-by: Hippolyte HENRY <zippolyte@users.noreply.github.com>

--- datadog/data_source_datadog_user.go
+++ datadog/data_source_datadog_user.go
@@ -1,9 +1,11 @@
 package datadog
 
 import (
-	"fmt"
+	"context"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	datadogV2 "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/internal/utils"
 )
@@ -11,7 +13,7 @@ import (
 func dataSourceDatadogUser() *schema.Resource {
 	return &schema.Resource{
 		Description: "Use this data source to retrieve information about an existing user to use it in an other resources.",
-		Read:        dataSourceDatadogUserRead,
+		ReadContext: dataSourceDatadogUserRead,
 
 		Schema: map[string]*schema.Schema{
 			"filter": {
@@ -20,8 +22,8 @@ func dataSourceDatadogUser() *schema.Resource {
 				Required:    true,
 			},
 			// Computed values
-			"id": {
-				Description: "Id of the user.",
+			"email": {
+				Description: "Email of the user.",
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
@@ -34,46 +36,34 @@ func dataSourceDatadogUser() *schema.Resource {
 	}
 }
 
-func dataSourceDatadogUserRead(d *schema.ResourceData, meta interface{}) error {
+func dataSourceDatadogUserRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	providerConf := meta.(*ProviderConfiguration)
 	datadogClientV2 := providerConf.DatadogClientV2
 	authV2 := providerConf.AuthV2
-	pageNumber := int64(0)
-	filter := d.Get("filter").(string)
-	found := false
+	filter := d.Get("filter").(string) // string | Filter all users by the given string. Defaults to no filtering. (optional) // string | Filter on status attribute. Comma separated list, with possible values `Active`, `Pending`, and `Disabled`. Defaults to no filtering. (optional)
+	optionalParams := datadogV2.ListUsersOptionalParameters{
+		Filter: &filter,
+	}
 
-	req := datadogClientV2.UsersApi.ListUsers(authV2).PageSize(20).PageNumber(pageNumber).Filter(filter)
-	res, _, err := req.Execute()
+	res, httpresp, err := datadogClientV2.UsersApi.ListUsers(authV2, optionalParams)
 	if err != nil {
-		return utils.TranslateClientError(err, "error querying user")
+		return utils.TranslateClientErrorDiag(err, httpresp, "error querying user")
 	}
-	totalPage := res.Meta.Page.GetTotalCount()
-	for pageNumber < totalPage {
-		req = req.PageNumber(pageNumber)
-		res, _, err := req.Execute()
-		if len(*res.Data) == 0 { // This will skip the empty calls to datadog api  ( 50 page in my org)
-			break
-		}
-		if err != nil {
-			return utils.TranslateClientError(err, "error querying user")
-		}
-		for _, user := range *res.Data {
-			if user.Attributes.GetEmail() == filter {
-				founded = true
-				d.SetId(user.Attributes.GetEmail())
-				if err := d.Set("name", user.Attributes.GetEmail()); err != nil {
-					return err
-				}
-				if err := d.Set("id", user.GetId()); err != nil {
-					return err
-				}
-				break
-			}
-		}
-		pageNumber++
+	users := res.GetData()
+	if len(users) > 1 {
+		return diag.Errorf("your query returned more than one result for filter \"%s\", please try a more specific search criteria",
+			filter,
+		)
+	} else if len(users) == 0 {
+		return diag.Errorf("didn't find any user matching filter string  \"%s\"", filter)
 	}
-	if !founded {
-		return fmt.Errorf("didn't find any user matching filter string  \"%s\"",)
+	matchedUser := users[0]
+	d.SetId(matchedUser.GetId())
+	if err := d.Set("name", matchedUser.Attributes.GetName()); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("email", matchedUser.Attributes.GetEmail()); err != nil {
+		return diag.FromErr(err)
 	}
 	return nil
 }
