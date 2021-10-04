5f4cbfcb9fffcc38bdbcfdda2d93c2776bc26b44
Spenser Pothier <spenser.pothier@automox.com>
2021-10-04 10:36:15 -0700

    Add support for creating monitors from JSON

--- datadog/provider.go
+++ datadog/provider.go
@@ -156,6 +156,7 @@ func Provider() *schema.Provider {
 			"datadog_metric_metadata":                      resourceDatadogMetricMetadata(),
 			"datadog_metric_tag_configuration":             resourceDatadogMetricTagConfiguration(),
 			"datadog_monitor":                              resourceDatadogMonitor(),
+			"datadog_monitor_json":                         resourceDatadogMonitorJSON(),
 			"datadog_organization_settings":                resourceDatadogOrganizationSettings(),
 			"datadog_role":                                 resourceDatadogRole(),
 			"datadog_security_monitoring_default_rule":     resourceDatadogSecurityMonitoringDefaultRule(),
