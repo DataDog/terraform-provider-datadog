3aaa0f38f4ea2708c52c6508be7f228a12ae9109
Andrea Piccione <andrea.piccione@datadoghq.com>
2021-10-04 09:57:42 +0100

    Remove security monitoring resource files exclusion from doc generation script

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 # Generate the documentation using tfplugindocs and remove changes to files that shouldn't change
 
-exclude_files=(
-  # There is an issue with the security_monitoring schema that requires the docs to be updated by hand if needed
-  'docs/data-sources/security_monitoring_rules.md'
-  'docs/resources/security_monitoring_default_rule.md'
-  'docs/resources/security_monitoring_rule.md'
-)
+# add here the files to be excluded from the doc generation
+exclude_files=()
 
 # Check if manual changes were made to any excluded files and exit
 # otherwise these will be lost with `tfplugindocs`
