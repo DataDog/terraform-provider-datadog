8aa9e85c450c056f26b8c4839b62353794d2373f
Spenser Pothier <spenser.pothier@automox.com>
2021-10-12 08:20:29 -0700

    PR Feedback: Add tests

--- datadog/resource_datadog_monitor_json.go
+++ datadog/resource_datadog_monitor_json.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
