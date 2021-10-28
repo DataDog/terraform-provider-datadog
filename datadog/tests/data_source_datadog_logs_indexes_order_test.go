b7ad44483aa81b213e1d4053518c3435f8f4c2b5
Julien Lamandé <jlamande@gmail.com>
2021-10-28 21:23:12 +0200

    Add datasource datadog_logs_indexes_order (#1244)
    * Add datasource datadog_logs_indexes_order
    
    * update cassettes and set resource ID
    
    * make docs
    
    Co-authored-by: Sherzod K <sherzod.karimov@datadoghq.com>

---
+++ datadog/tests/data_source_datadog_logs_indexes_order_test.go
@@ -0,0 +1,65 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDatadogLogsIndexesOrderDatasource(t *testing.T) {
+	if !isReplaying() {
+		// Skip in non replaying mode, since we can't delete indexes via the public API, plus the API takes a while to be consistent
+		// If you really need to record the interactions, comment the following return statement, and run locally.
+		log.Println("Skipping logs indexes tests in non replaying mode")
+		return
+	}
+
+	ctx, accProviders := testAccProviders(context.Background(), t)
+	uniq := strings.ToLower(strings.ReplaceAll(uniqueEntityName(ctx, t), "_", "-"))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceDatadogLogsIndexesOrderWithIndexConfig(uniq),
+				// given the persistent nature of log indexes we can't check for exact value
+				Check: resource.TestMatchResourceAttr("data.datadog_logs_indexes_order.order", "index_names.#", regexp.MustCompile("[^0].*$")),
+			},
+			{
+				Config: testAccDatasourceDatadogLogsIndexesOrderConfig(),
+				// given the persistent nature of log indexes we can't check for exact value
+				// we can assume that at least one index already exists given the previous test
+				Check: resource.TestMatchResourceAttr("data.datadog_logs_indexes_order.order", "index_names.#", regexp.MustCompile("[^0].*$")),
+			},
+		},
+	})
+}
+
+func testAccDatasourceDatadogLogsIndexesOrderConfig() string {
+	return `
+data "datadog_logs_indexes_order" "order" {}
+`
+}
+
+func testAccDatasourceDatadogLogsIndexesOrderWithIndexConfig(uniq string) string {
+	return fmt.Sprintf(`
+data "datadog_logs_indexes_order" "order" {
+	depends_on = [
+		datadog_logs_index.sample_index,
+	]
+}
+
+resource "datadog_logs_index" "sample_index" {
+	name           = "%s-sample"
+	filter {
+		query = "project:sample"
+	}
+}
+`, uniq)
+}
