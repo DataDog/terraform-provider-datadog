3242629dd1585a30668cfe92cbc4643d977987d8
skarimo <40482491+skarimo@users.noreply.github.com>
2022-01-11 09:29:22 -0500

    fix monitor tests (#1323)

--- datadog/tests/resource_datadog_monitor_json_test.go
+++ datadog/tests/resource_datadog_monitor_json_test.go
@@ -50,7 +50,7 @@ func TestAccDatadogMonitorJSONBasic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					checkDashboardExists(accProvider),
 					resource.TestCheckResourceAttr(
-						"datadog_monitor_json.monitor_json", "monitor", fmt.Sprintf("{\"message\":\"Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\\n\\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.\",\"name\":\"%s\",\"options\":{\"include_tags\":true,\"locked\":false,\"new_host_delay\":150,\"notify_audit\":false,\"notify_no_data\":false,\"thresholds\":{\"critical\":1,\"ok\":1,\"warning\":1}},\"priority\":null,\"query\":\"\\\"ntp.in_sync\\\".over(\\\"*\\\").last(2).count_by_status()\",\"tags\":[],\"type\":\"service check\"}", uniq)),
+						"datadog_monitor_json.monitor_json", "monitor", fmt.Sprintf("{\"message\":\"Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\\n\\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.\",\"name\":\"%s\",\"options\":{\"include_tags\":true,\"locked\":false,\"new_host_delay\":150,\"notify_audit\":false,\"notify_no_data\":false,\"thresholds\":{\"critical\":1,\"ok\":1,\"warning\":1}},\"priority\":null,\"query\":\"\\\"ntp.in_sync\\\".by(\\\"*\\\").last(2).count_by_status()\",\"tags\":[],\"type\":\"service check\"}", uniq)),
 				),
 			},
 			{
@@ -58,7 +58,7 @@ func TestAccDatadogMonitorJSONBasic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					checkDashboardExists(accProvider),
 					resource.TestCheckResourceAttr(
-						"datadog_monitor_json.monitor_json", "monitor", fmt.Sprintf("{\"message\":\"Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\\n\\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.\",\"name\":\"%s\",\"options\":{\"include_tags\":true,\"locked\":false,\"new_host_delay\":150,\"notify_audit\":false,\"notify_no_data\":false,\"thresholds\":{\"critical\":1,\"ok\":1,\"warning\":1}},\"priority\":null,\"query\":\"\\\"ntp.in_sync\\\".over(\\\"*\\\").last(2).count_by_status()\",\"tags\":[],\"type\":\"service check\"}", uniqUpdated)),
+						"datadog_monitor_json.monitor_json", "monitor", fmt.Sprintf("{\"message\":\"Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\\n\\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.\",\"name\":\"%s\",\"options\":{\"include_tags\":true,\"locked\":false,\"new_host_delay\":150,\"notify_audit\":false,\"notify_no_data\":false,\"thresholds\":{\"critical\":1,\"ok\":1,\"warning\":1}},\"priority\":null,\"query\":\"\\\"ntp.in_sync\\\".by(\\\"*\\\").last(2).count_by_status()\",\"tags\":[],\"type\":\"service check\"}", uniqUpdated)),
 				),
 			},
 		},
@@ -72,7 +72,7 @@ resource "datadog_monitor_json" "monitor_json" {
 {
     "name": "%s",
     "type": "service check",
-    "query": "\"ntp.in_sync\".over(\"*\").last(2).count_by_status()",
+    "query": "\"ntp.in_sync\".by(\"*\").last(2).count_by_status()",
     "message": "Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\n\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.",
     "tags": [],
     "multi": true,
