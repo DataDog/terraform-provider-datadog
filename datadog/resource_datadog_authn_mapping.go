fe05073e0a0199057f00a1af4643c5e0f82c1929
Swetha Murali <91908764+swetha-murali-dd@users.noreply.github.com>
2022-02-08 15:33:06 -0500

    ACCESS 599: Provider for creating AuthN Mappings (#1344)
    * Build skeleton for Datadog SAML Role Mappings terraform provider flow.
    
    * Create role mapping
    
    * Update state and add example
    
    * Add terraform test
    
    * Create a dummy check destroy handler
    
    * Changes as per PR review
    
    Co-authored-by: Alexa Liaskovski <alexa.liaskovski@datadoghq.com>

--- datadog/resource_datadog_authn_mapping.go
+++ datadog/resource_datadog_authn_mapping.go
@@ -2,14 +2,20 @@ package datadog
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
+	"github.com/DataDog/datadog-api-client-go/api/v2/datadog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/terraform-providers/terraform-provider-datadog/datadog/internal/utils"
 )
 
 func resourceDatadogAuthnMapping() *schema.Resource {
 	return &schema.Resource{
-		Description:   "Provides a Datadog SAML Role Mappings resource. This can be used to create and manage Datadog SAML Role Mappings.",
+		Description:   "Provides a Datadog AuthN Mappings resource. This can be used to create and manage Datadog SAML Mappings.",
 		CreateContext: resourceDatadogAuthnMappingCreate,
 		ReadContext:   resourceDatadogAuthnMappingRead,
 		UpdateContext: resourceDatadogAuthnMappingUpdate,
@@ -36,6 +42,42 @@ func resourceDatadogAuthnMapping() *schema.Resource {
 }
 
 func resourceDatadogAuthnMappingCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*ProviderConfiguration).DatadogClientV2
+	auth := meta.(*ProviderConfiguration).AuthV2
+	authNMapReq := buildAuthNMappingCreateRequest(d)
+	createResp, httpResponse, err := client.AuthNMappingsApi.CreateAuthNMapping(auth, authNMapReq)
+	if err != nil {
+		return utils.TranslateClientErrorDiag(err, httpResponse, "error creating authn mapping")
+	}
+	if err := utils.CheckForUnparsed(createResp); err != nil {
+		return diag.FromErr(err)
+	}
+
+	var getAuthNMappingResponse datadog.AuthNMappingResponse
+	var httpResponseGet *http.Response
+
+	err = resource.RetryContext(ctx, d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
+		getAuthNMappingResponse, httpResponseGet, err = client.AuthNMappingsApi.GetAuthNMapping(auth, createResp.Data.GetId())
+		if err != nil {
+			if httpResponseGet != nil && httpResponseGet.StatusCode == 404 {
+				return resource.RetryableError(fmt.Errorf("SAML role mapping not created yet"))
+			}
+
+			return resource.NonRetryableError(err)
+		}
+		if err := utils.CheckForUnparsed(getAuthNMappingResponse); err != nil {
+			return resource.NonRetryableError(err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	authNMappingData := getAuthNMappingResponse.GetData()
+	d.SetId(authNMappingData.GetId())
+	updateAuthNMappingState(d, &authNMappingData)
 	return nil
 }
 
@@ -49,4 +91,47 @@ func resourceDatadogAuthnMappingUpdate(ctx context.Context, d *schema.ResourceDa
 
 func resourceDatadogAuthnMappingDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	return nil
+}
+
+func updateAuthNMappingState(d *schema.ResourceData, authNMapping *datadog.AuthNMapping) diag.Diagnostics {
+	authNMappingAttributes := authNMapping.GetAttributes()
+	authNMappingRelations := authNMapping.GetRelationships()
+	authNMappingRoleRelation := authNMappingRelations.GetRole()
+	authNRole := authNMappingRoleRelation.GetData()
+	if err := d.Set("key", authNMappingAttributes.GetAttributeKey()); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("value", authNMappingAttributes.GetAttributeValue()); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("role", authNRole.GetId()); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
+
+func buildAuthNMappingCreateRequest(d *schema.ResourceData) datadog.AuthNMappingCreateRequest {
+	authNMappingCreateRequest := datadog.NewAuthNMappingCreateRequestWithDefaults()
+	authNMappingCreateData := datadog.NewAuthNMappingCreateDataWithDefaults()
+	authNMappingCreateAttrs := datadog.NewAuthNMappingCreateAttributesWithDefaults()
+	authNMappingRelations := datadog.NewAuthNMappingCreateRelationshipsWithDefaults()
+
+	// Set AuthN mapping Attributes
+	authNMappingCreateAttrs.SetAttributeKey(d.Get("key").(string))
+	authNMappingCreateAttrs.SetAttributeValue(d.Get("value").(string))
+
+	// Set AuthN mapping Relationships
+	roleRelations := datadog.NewRelationshipToRoleWithDefaults()
+	roleRelationsData := datadog.NewRelationshipToRoleDataWithDefaults()
+	roleRelationsData.SetId(d.Get("role").(string))
+	roleRelations.SetData(*roleRelationsData)
+	authNMappingRelations.SetRole(*roleRelations)
+
+	// Set AuthN mapping create data
+	authNMappingCreateData.SetAttributes(*authNMappingCreateAttrs)
+	authNMappingCreateData.SetRelationships(*authNMappingRelations)
+
+	// Set AuthN mapping create request
+	authNMappingCreateRequest.SetData(*authNMappingCreateData)
+	return *authNMappingCreateRequest
 }
