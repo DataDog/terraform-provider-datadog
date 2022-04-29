a8fac47c47e63cfad1c067b7e487830282817883
David Forsythe <david.forsythe@datadoghq.com>
2022-04-29 16:35:04 +0200

    [Synthetics] Change PL schema to TypeSet for tests

--- datadog/resource_datadog_synthetics_test_.go
+++ datadog/resource_datadog_synthetics_test_.go
@@ -485,7 +485,7 @@ func syntheticsTestOptionsList() *schema.Schema {
 				},
 				"restricted_roles": {
 					Description: "A list of role identifiers pulled from the Roles API to restrict read and write access.",
-					Type:        schema.TypeList,
+					Type:        schema.TypeSet,
 					Optional:    true,
 					Elem:        &schema.Schema{Type: schema.TypeString},
 				},
