aef239810d7c5ec553200b1f852126d8e8adff7f
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2022-08-16 14:10:10 -0400

    update example

--- examples/guides/watchdog_monitor.tf
+++ examples/guides/watchdog_monitor.tf
@@ -1,10 +1,10 @@
 resource "datadog_monitor" "watchdog_monitor" {
   name               = "Watchdog Monitor TF"
-  type               = "event alert"
+  type               = "event-v2 alert"
   message            = "Check out this Watchdog Service Monitor"
   escalation_message = "Escalation message"
 
-  query = "events('priority:all sources:watchdog tags:story_type:service,env:test_env,service:test_service:_aggregate').by('service,resource_name').rollup('count').last('30m') > 0"
+  query = "events(\"priority:all sources:watchdog tags:(story_type:service env:test_env service:testservice_aggregate)\").rollup(\"count\").by(\"service,resource_name\").last(\"30m\") > 0"
 
   include_tags = true
 
