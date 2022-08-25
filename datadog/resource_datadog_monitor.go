2ef87840df40901acf0632cbf8e1005222cdd53a
Chris Matteri <chris.matteri@datadoghq.com>
2022-08-25 15:45:00 -0700

    Apply doc updates to source

--- datadog/resource_datadog_monitor.go
+++ datadog/resource_datadog_monitor.go
@@ -177,7 +177,7 @@ func resourceDatadogMonitor() *schema.Resource {
 				ConflictsWith: []string{"on_missing_data"},
 			},
 			"on_missing_data": {
-				Description:   "Controls how groups or monitors are treated if an evaluation does not return any data points. The default option results in different behavior depending on the monitor query type. For monitors using Count queries, an empty monitor evaluation is treated as 0 and is compared to the threshold conditions. For monitors using any query type other than Count, for example Gauge, Measure, or Rate, the monitor shows the last known status. This option is only available for APM Trace Analytics, Audit Trail, CI, Error Tracking, Event, Logs, and RUM monitors. Valid values are: \"show_no_data\", \"show_and_notify_no_data\", \"resolve\", and \"default\"",
+				Description:   "Controls how groups or monitors are treated if an evaluation does not return any data points. The default option results in different behavior depending on the monitor query type. For monitors using `Count` queries, an empty monitor evaluation is treated as 0 and is compared to the threshold conditions. For monitors using any query type other than `Count`, for example `Gauge`, `Measure`, or `Rate`, the monitor shows the last known status. This option is only available for APM Trace Analytics, Audit Trail, CI, Error Tracking, Event, Logs, and RUM monitors. Valid values are: `show_no_data`, `show_and_notify_no_data`, `resolve`, and `default`.",
 				Type:          schema.TypeString,
 				Optional:      true,
 				ConflictsWith: []string{"notify_no_data", "no_data_timeframe"},
