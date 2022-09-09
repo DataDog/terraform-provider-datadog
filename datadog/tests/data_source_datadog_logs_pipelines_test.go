8ba347ca5ccc7dc29013eaccef3fb92ae27fe2e5
Kevin Zou <kevin.zou@datadoghq.com>
2022-09-09 10:41:52 -0400

    add tests for readonly pipelines data source

--- datadog/tests/data_source_datadog_logs_pipelines_test.go
+++ datadog/tests/data_source_datadog_logs_pipelines_test.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
@@ -60,6 +61,64 @@ func dataLogsPipelinesCountCheck(accProvider func() (*schema.Provider, error)) f
 		if logPipelinesCount != len(logsPipelines) {
 			return fmt.Errorf("expected %d pipelines got %d pipelines",
 				logPipelinesCount, len(logsPipelines))
+		}
+
+		return nil
+	}
+}
+
+func TestAccDatadogLogsPipelinesDatasourceReadonly(t *testing.T) {
+	_, accProviders := testAccProviders(context.Background(), t)
+	accProvider := testAccProvider(t, accProviders)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceLogsPipelinesReadonlyConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					dataLogsPipelinesReadonlyCountCheck(accProvider),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourceLogsPipelinesReadonlyConfig() string {
+	return `
+data "datadog_logs_pipelines" "foo" {
+	is_read_only = true
+}`
+}
+
+func dataLogsPipelinesReadonlyCountCheck(accProvider func() (*schema.Provider, error)) func(state *terraform.State) error {
+	return func(state *terraform.State) error {
+		provider, _ := accProvider()
+		providerConf := provider.Meta().(*datadog.ProviderConfiguration)
+		auth := providerConf.Auth
+		apiInstances := providerConf.DatadogApiInstances
+
+		logsPipelines, _, err := apiInstances.GetLogsPipelinesApiV1().ListLogsPipelines(auth)
+		var readOnlyPipelines []datadogV1.LogsPipeline
+		for _, pipeline := range logsPipelines {
+			if pipeline.GetIsReadOnly() {
+				readOnlyPipelines = append(readOnlyPipelines, pipeline)
+			}
+		}
+		if err != nil {
+			return err
+		}
+		if err := utils.CheckForUnparsed(logsPipelines); err != nil {
+			return err
+		}
+
+		resourceAttributes := state.RootModule().Resources["data.datadog_logs_pipelines.foo"].Primary.Attributes
+		logPipelinesCount, _ := strconv.Atoi(resourceAttributes["logs_pipelines.#"])
+
+		if logPipelinesCount != len(readOnlyPipelines) {
+			return fmt.Errorf("expected %d pipelines got %d pipelines",
+				logPipelinesCount, len(readOnlyPipelines))
 		}
 
 		return nil
