a6c13b7066e0fab09255c2bdbd617bc3717640b2
Kevin Zou <kevin.zou@datadoghq.com>
2022-09-13 13:31:02 -0400

    add example of using pipelines datasource in pipelines_order resource

---
+++ examples/data-sources/datadog_logs_pipelines/data-source.tf
@@ -0,0 +1,11 @@
+# Gets all pipelines that are not read-only
+data "datadog_logs_pipelines" "pipelines" {
+  is_read_only = "false"
+}
+
+# Using data source to set pipeline order
+resource "datadog_logs_pipeline_order" "lpo" {
+  name = "lpo"
+  pipelines = [for pipeline in 
+    (data.datadog_logs_pipelines.pipelines.logs_pipelines) : pipeline.id]
+}
