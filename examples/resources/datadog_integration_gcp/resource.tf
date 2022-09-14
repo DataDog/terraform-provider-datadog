876b0ae34770f2708d503f7b6ab927c59fd31c99
Jack Ketcham <jack@jackketcham.com>
2022-09-14 07:56:05 -0700

    Add gcp integration example w/ google_service_account_key (#1428)

--- examples/resources/datadog_integration_gcp/resource.tf
+++ examples/resources/datadog_integration_gcp/resource.tf
@@ -8,3 +8,21 @@ resource "datadog_integration_gcp" "awesome_gcp_project_integration" {
   host_filters   = "foo:bar,buzz:lightyear"
 }
 
+
+# Usage with google_service_account and google_service_account_key resources
+resource "google_service_account" "datadog" {
+  account_id   = "datadog-integration"
+  display_name = "Datadog Integration"
+}
+
+resource "google_service_account_key" "datadog" {
+  service_account_id = google_service_account.datadog.name
+}
+
+resource "datadog_integration_gcp" "awesome_gcp_project_integration" {
+  project_id     = jsondecode(base64decode(google_service_account_key.datadog.private_key))["project_id"]
+  private_key    = jsondecode(base64decode(google_service_account_key.datadog.private_key))["private_key"]
+  private_key_id = jsondecode(base64decode(google_service_account_key.datadog.private_key))["private_key_id"]
+  client_email   = jsondecode(base64decode(google_service_account_key.datadog.private_key))["client_email"]
+  client_id      = jsondecode(base64decode(google_service_account_key.datadog.private_key))["client_id"]
+}
