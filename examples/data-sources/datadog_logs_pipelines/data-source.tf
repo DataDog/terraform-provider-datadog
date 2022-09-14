290517ab07dd2a8f393486ac790a997abcd23cb7
Kevin Zou <kevin.zou@datadoghq.com>
2022-09-14 10:36:35 -0400

    remove flag to keep example runnable

--- examples/data-sources/datadog_logs_pipelines/data-source.tf
+++ examples/data-sources/datadog_logs_pipelines/data-source.tf
@@ -1,7 +1,5 @@
-# Gets all pipelines that are not read-only
-data "datadog_logs_pipelines" "pipelines" {
-  is_read_only = "false"
-}
+# Gets all pipelines
+data "datadog_logs_pipelines" "pipelines" {}
 
 # Using data source to set pipeline order
 resource "datadog_logs_pipeline_order" "lpo" {
