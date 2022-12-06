b33a8018e468aaea4efb1ebb8345b7a2f4afc891
Kevin Zou <kevin.zou@datadoghq.com>
2022-12-06 16:42:23 -0500

    add test case that tests existence of new archive in archive order

--- datadog/tests/data_source_datadog_logs_archives_order_test.go
+++ datadog/tests/data_source_datadog_logs_archives_order_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"context"
+	"fmt"
 	"regexp"
 	"testing"
 
@@ -9,7 +10,8 @@ import (
 )
 
 func TestAccDatadogLogsArchivesOrderDatasource(t *testing.T) {
-	_, accProviders := testAccProviders(context.Background(), t)
+	ctx, accProviders := testAccProviders(context.Background(), t)
+	uniq := uniqueAWSAccountID(ctx, t)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
@@ -19,6 +21,40 @@ func TestAccDatadogLogsArchivesOrderDatasource(t *testing.T) {
 				Config: `data "datadog_logs_archives_order" "order" {}`,
 				Check:  resource.TestMatchResourceAttr("data.datadog_logs_archives_order.order", "archive_ids.#", regexp.MustCompile("[^0].*$")),
 			},
+			{
+				Config: testAccDatasourceDatadogLogsArchiveOrderWithArchive(uniq),
+				Check:  resource.TestCheckTypeSetElemAttrPair("data.datadog_logs_archives_order.order", "archive_ids.*", "datadog_logs_archive.sample_archive", "id"),
+			},
 		},
 	})
+}
+
+func testAccDatasourceDatadogLogsArchiveOrderWithArchive(uniq string) string {
+	return fmt.Sprintf(`
+data "datadog_logs_archives_order" "order" {
+	depends_on = [
+		datadog_logs_archive.sample_archive,
+	]
+}
+
+resource "datadog_integration_aws" "account" {
+	account_id         = "%[1]s"
+	role_name          = "testacc-datadog-integration-role"
+}
+
+resource "datadog_logs_archive" "sample_archive" {
+	depends_on = ["datadog_integration_aws.account"]
+	name = "my first s3 archive"
+	query = "service:tutu"
+	s3_archive {
+		bucket 		 = "my-bucket"
+		path 		 = "/path/foo"
+		account_id   = "%[1]s"
+		role_name    = "testacc-datadog-integration-role"
+	}
+	rehydration_tags = ["team:intake", "team:app"]
+	include_tags = true
+	rehydration_max_scan_size_in_gb = 123
+}
+`, uniq)
 }
