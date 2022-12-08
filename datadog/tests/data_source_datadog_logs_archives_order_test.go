a492c76783e7d98b94fc1038ab18362f1b96511e
Kevin Zou <17015060+nkzou@users.noreply.github.com>
2022-12-08 09:04:56 -0500

    [datadog_logs_archive_order] Add a logs archive order data source (#1661)
    * add logs archive order data source
    
    * add simple test for logs archive order data source
    
    * add cassettes for logs archive order test
    
    * Apply suggestions from code review
    
    Co-authored-by: skarimo <40482491+skarimo@users.noreply.github.com>
    
    * make docs
    
    * add test case that tests existence of new archive in archive order
    
    * change check to compare count between api call and terraform state
    
    Co-authored-by: skarimo <40482491+skarimo@users.noreply.github.com>

---
+++ datadog/tests/data_source_datadog_logs_archives_order_test.go
@@ -0,0 +1,90 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/terraform-providers/terraform-provider-datadog/datadog"
+)
+
+func logsArchiveOrderCheckCount(accProvider func() (*schema.Provider, error)) func(state *terraform.State) error {
+	return func(state *terraform.State) error {
+		provider, _ := accProvider()
+		providerConf := provider.Meta().(*datadog.ProviderConfiguration)
+		auth := providerConf.Auth
+		apiInstances := providerConf.DatadogApiInstances
+
+		logsArchiveOrder, _, err := apiInstances.GetLogsArchivesApiV2().GetLogsArchiveOrder(auth)
+		if err != nil {
+			return err
+		}
+		return logsArchiveOrderCount(state, len(logsArchiveOrder.Data.Attributes.ArchiveIds))
+	}
+}
+
+func logsArchiveOrderCount(state *terraform.State, responseCount int) error {
+	resourceAttributes := state.RootModule().Resources["data.datadog_logs_archives_order.order"].Primary.Attributes
+	logsArchiveCount, _ := strconv.Atoi(resourceAttributes["archive_ids.#"])
+
+	if logsArchiveCount != responseCount {
+		return fmt.Errorf("expected %d logs archives in order, got %d logs archives",
+			responseCount, logsArchiveCount)
+	}
+	return nil
+}
+
+func TestAccDatadogLogsArchivesOrderDatasource(t *testing.T) {
+	ctx, accProviders := testAccProviders(context.Background(), t)
+	accProvider := testAccProvider(t, accProviders)
+	uniq := uniqueAWSAccountID(ctx, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: `data "datadog_logs_archives_order" "order" {}`,
+				Check:  resource.TestMatchResourceAttr("data.datadog_logs_archives_order.order", "archive_ids.#", regexp.MustCompile("[^0].*$")),
+			},
+			{
+				Config: testAccDatasourceDatadogLogsArchiveOrderWithArchive(uniq),
+				Check: resource.ComposeTestCheckFunc(
+					logsArchiveOrderCheckCount(accProvider),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourceDatadogLogsArchiveOrderWithArchive(uniq string) string {
+	return fmt.Sprintf(`
+data "datadog_logs_archives_order" "order" {
+	depends_on = ["datadog_logs_archive.sample_archive"]
+}
+
+resource "datadog_integration_aws" "account" {
+	account_id         = "%[1]s"
+	role_name          = "testacc-datadog-integration-role"
+}
+
+resource "datadog_logs_archive" "sample_archive" {
+	name = "my first s3 archive"
+	query = "service:tutu"
+	s3_archive {
+		bucket 		 = "my-bucket"
+		path 		 = "/path/foo"
+		account_id   = datadog_integration_aws.account.account_id
+		role_name    = "testacc-datadog-integration-role"
+	}
+	rehydration_tags = ["team:intake", "team:app"]
+	include_tags = true
+	rehydration_max_scan_size_in_gb = 123
+}
+`, uniq)
+}
