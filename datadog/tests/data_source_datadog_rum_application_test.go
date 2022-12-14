a0ddfe31336d61cb63ec83e3fd6dd373b6dd77f5
Kevin Zou <17015060+nkzou@users.noreply.github.com>
2022-12-14 14:30:15 -0500

    [datadog_rum_application] Add a RUM application data source (#1641)
    * Add RUM application data source
    
    * add search by name/type, test cases, and cassettes
    
    * fix/refactor name and type matching conditional
    
    * update data source and resource to follow new rum api spec
    
    * go mod tidy
    
    * update rum app resource test
    
    * update test and data source
    
    * make docs
    
    * add mutex for RUM resource creation, update cassettes
    
    * remove mutex for performance as bug was fixed on backend

---
+++ datadog/tests/data_source_datadog_rum_application_test.go
@@ -0,0 +1,139 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDatadogRUMApplicationDatasourceNameFilter(t *testing.T) {
+	ctx, accProviders := testAccProviders(context.Background(), t)
+	uniq := uniqueEntityName(ctx, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceRUMApplicationNameFilterConfig(uniq),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.datadog_rum_application.bar", "name", uniq),
+					resource.TestCheckResourceAttr(
+						"data.datadog_rum_application.bar", "type", "browser"),
+					resource.TestCheckResourceAttrPair(
+						"data.datadog_rum_application.bar", "id", "datadog_rum_application.foo", "id"),
+					resource.TestCheckResourceAttrPair(
+						"data.datadog_rum_application.bar", "client_token", "datadog_rum_application.foo", "client_token"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDatadogRUMApplicationDatasourceIDFilter(t *testing.T) {
+	ctx, accProviders := testAccProviders(context.Background(), t)
+	uniq := uniqueEntityName(ctx, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceRUMApplicationIDConfig(uniq),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.datadog_rum_application.bar", "name", uniq),
+					resource.TestCheckResourceAttr(
+						"data.datadog_rum_application.bar", "type", "browser"),
+					resource.TestCheckResourceAttrPair(
+						"data.datadog_rum_application.bar", "id", "datadog_rum_application.foo", "id"),
+					resource.TestCheckResourceAttrPair(
+						"data.datadog_rum_application.bar", "client_token", "datadog_rum_application.foo", "client_token"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDatadogRUMApplicationDatasourceErrorMultiple(t *testing.T) {
+	ctx, accProviders := testAccProviders(context.Background(), t)
+	uniq := uniqueEntityName(ctx, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccDatasourceRUMApplicationMultiple(uniq),
+				ExpectError: regexp.MustCompile("more than one RUM application"),
+			},
+		},
+	})
+}
+
+func testAccDatasourceRUMApplicationNameFilterConfig(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_rum_application" "foo" {
+	name = "%[1]s"
+	type = "browser"
+}
+
+resource "datadog_rum_application" "baz" {
+	name = "%[1]s-extra"
+	type = "browser"
+}
+
+data "datadog_rum_application" "bar" {
+	depends_on = [
+		datadog_rum_application.foo,
+		datadog_rum_application.baz,
+	]
+    name_filter = "%[1]s"
+}`, uniq)
+}
+
+func testAccDatasourceRUMApplicationIDConfig(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_rum_application" "foo" {
+	name = "%[1]s"
+	type = "browser"
+}
+
+resource "datadog_rum_application" "baz" {
+	name = "%[1]s"
+	type = "browser"
+}
+
+data "datadog_rum_application" "bar" {
+	depends_on = [
+		datadog_rum_application.foo,
+		datadog_rum_application.baz,
+	]
+    id = datadog_rum_application.foo.id
+}`, uniq)
+}
+
+func testAccDatasourceRUMApplicationMultiple(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_rum_application" "foo" {
+	name = "%[1]s"
+	type = "browser"
+}
+
+resource "datadog_rum_application" "baz" {
+	name = "%[1]s"
+	type = "browser"
+}
+
+data "datadog_rum_application" "bar" {
+	depends_on = [
+		datadog_rum_application.foo,
+		datadog_rum_application.baz,
+	]
+    name_filter = "%[1]s"
+}`, uniq)
+}
