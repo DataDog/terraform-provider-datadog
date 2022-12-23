c4c720fc26d1b835bcb9ceed7e94b6010c81fd22
Mike Nguyen <mike.nguyen@datadoghq.com>
2022-12-23 10:21:31 -0500

    Add update test

--- datadog/tests/resource_datadog_service_account_test.go
+++ datadog/tests/resource_datadog_service_account_test.go
@@ -30,7 +30,6 @@ func TestServiceAccountCreate(t *testing.T) {
           roles = [data.datadog_role.ro_role.id]
         }`),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDatadogUserV2Exists(accProvider, "datadog_user.foo"),
 					resource.TestCheckResourceAttr(
 						"datadog_service_account.some_test_service_account", "email", "some_linked_users_email@test.com"),
 					resource.TestCheckResourceAttr(
@@ -38,7 +37,65 @@ func TestServiceAccountCreate(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"datadog_service_account.some_test_service_account", "disabled", "false"),
 					resource.TestCheckResourceAttr(
-						"datadog_service_account.some_test_service_account", "roles", "[]"),
+						"datadog_service_account.some_test_service_account", "roles.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestServiceAccountUpdate(t *testing.T) {
+	t.Parallel()
+	_, accProviders := testAccProviders(context.Background(), t)
+	accProvider := testAccProvider(t, accProviders)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		CheckDestroy:      testAccCheckDatadogUserV2Destroy(accProvider),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+        data "datadog_role" "ro_role" {
+          filter = "Datadog Read Only Role"
+        }
+
+        resource "datadog_service_account" "some_test_updated_service_account" {
+          email = "some_linked_users_email@test.com"
+          name = "Service account linked to some user"
+          roles = [data.datadog_role.ro_role.id]
+        }`),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "email", "some_linked_users_email@test.com"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "name", "Service account linked to some user"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "disabled", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "roles.#", "1"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+        data "datadog_role" "ro_role" {
+          filter = "Datadog Read Only Role"
+        }
+
+        resource "datadog_service_account" "some_test_updated_service_account" {
+          email = "some_linked_users_email@test.com"
+          name = "New name for the service account"
+          roles = []
+        }`),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "email", "some_linked_users_email@test.com"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "name", "New name for the service account"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "disabled", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.some_test_updated_service_account", "roles.#", "0"),
 				),
 			},
 		},
