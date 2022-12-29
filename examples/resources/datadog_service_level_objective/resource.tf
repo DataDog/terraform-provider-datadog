4f362f3045f08563d334e84dcda98aacbd840c61
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2022-12-29 13:32:01 -0500

    fix broken example

--- examples/resources/datadog_service_level_objective/resource.tf
+++ examples/resources/datadog_service_level_objective/resource.tf
@@ -13,16 +13,12 @@ resource "datadog_service_level_objective" "foo" {
     timeframe       = "7d"
     target          = 99.9
     warning         = 99.99
-    target_display  = "99.900"
-    warning_display = "99.990"
   }
 
   thresholds {
     timeframe       = "30d"
     target          = 99.9
     warning         = 99.99
-    target_display  = "99.900"
-    warning_display = "99.990"
   }
 
   tags = ["foo:bar", "baz"]
