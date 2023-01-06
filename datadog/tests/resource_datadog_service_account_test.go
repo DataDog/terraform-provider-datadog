4c84cca4c0ec1cb76aeddeb1cc8e89ab3ce5bea6
Mike Nguyen <104788383+mnguyendatadog@users.noreply.github.com>
2023-01-06 11:24:59 -0500

    [datadog_service_account] Add service account resource (#1685)
    * Add service account provider
    
    * Add a test and hook up span tagging
    
    * Add update test
    
    * Add test service account cassettes
    
    * Add docs and example
    
    * Make docs
    
    * Tighten up tests, delete unused code
    
    * make docs
    
    * Add import section to doc
    
    * Add import example
    
    Co-authored-by: Kevin Zou <kevin.zou@datadoghq.com>

---
+++ datadog/tests/resource_datadog_service_account_test.go
@@ -0,0 +1,92 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestServiceAccountCreate(t *testing.T) {
+	t.Parallel()
+	_, accProviders := testAccProviders(context.Background(), t)
+	accProvider := testAccProvider(t, accProviders)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		CheckDestroy:      testAccCheckDatadogUserV2Destroy(accProvider),
+		Steps: []resource.TestStep{
+			{
+				Config: generateServiceAccountConfig("some_linked_users_email@test.com", "Service account linked to some user", []string{"data.datadog_role.ro_role.id"}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "email", "some_linked_users_email@test.com"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "name", "Service account linked to some user"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "disabled", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "roles.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestServiceAccountUpdate(t *testing.T) {
+	t.Parallel()
+	_, accProviders := testAccProviders(context.Background(), t)
+	accProvider := testAccProvider(t, accProviders)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		CheckDestroy:      testAccCheckDatadogUserV2Destroy(accProvider),
+		Steps: []resource.TestStep{
+			{
+				Config: generateServiceAccountConfig("some_linked_users_email@test.com", "Service account linked to some user", []string{"data.datadog_role.ro_role.id"}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "email", "some_linked_users_email@test.com"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "name", "Service account linked to some user"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "disabled", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "roles.#", "1"),
+				),
+			},
+			{
+				Config: generateServiceAccountConfig("some_linked_users_email@test.com", "New name for the service account", []string{}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "email", "some_linked_users_email@test.com"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "name", "New name for the service account"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "disabled", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_service_account.automated_test_service_account", "roles.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+// Generates a terraform config with a read only role and single service account
+func generateServiceAccountConfig(email, name string, roles []string) string {
+	return fmt.Sprintf(`
+        data "datadog_role" "ro_role" {
+          filter = "Datadog Read Only Role"
+        }
+
+        resource "datadog_service_account" "automated_test_service_account" {
+          email = "%v"
+          name = "%v"
+          roles = [%v]
+        }
+  `, email, name, strings.Join(roles, ","))
+}
