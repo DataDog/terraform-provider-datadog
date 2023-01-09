42df7f4acaf7cc7b4df9c91badcfba6082c6f887
Kevin Zou <17015060+nkzou@users.noreply.github.com>
2023-01-09 11:02:52 -0500

    [datadog_integration_aws_logs_services] Add an AWS log ready services data source (#1690)
    * Add data source for AWS logs services
    
    * add labels to aws logs service data source, update tests
    
    * update descriptions
    
    * make docs
    
    * rename data source to be in line with other aws integration resources
    
    * make docs
    
    * update function names with datadog integration naming convention
    
    * rerecord cassettes
    
    * Apply suggestions from code review
    
    Co-authored-by: skarimo <40482491+skarimo@users.noreply.github.com>
    
    * make fmt
    
    Co-authored-by: Adam Stirk <17902029+Maddog2050@users.noreply.github.com>
    Co-authored-by: skarimo <40482491+skarimo@users.noreply.github.com>

---
+++ datadog/tests/data_source_datadog_integration_aws_logs_services_test.go
@@ -0,0 +1,62 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/terraform-providers/terraform-provider-datadog/datadog"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccDatadogIntegrationAWSLogsServicesDatasource(t *testing.T) {
+	_, accProviders := testAccProviders(context.Background(), t)
+	accProvider := testAccProvider(t, accProviders)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceIntegrationAWSLogsServicesConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					checkDatadogIntegrationAWSLogsServicesCount(accProvider),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourceIntegrationAWSLogsServicesConfig() string {
+	return `
+data "datadog_integration_aws_logs_services" "foo" {
+}`
+}
+
+func checkDatadogIntegrationAWSLogsServicesCount(accProvider func() (*schema.Provider, error)) func(state *terraform.State) error {
+	return func(state *terraform.State) error {
+		provider, _ := accProvider()
+		providerConf := provider.Meta().(*datadog.ProviderConfiguration)
+		auth := providerConf.Auth
+		apiInstances := providerConf.DatadogApiInstances
+
+		awsLogsServices, _, err := apiInstances.GetAWSLogsIntegrationApiV1().ListAWSLogsServices(auth)
+		if err != nil {
+			return err
+		}
+
+		resourceAttributes := state.RootModule().Resources["data.datadog_integration_aws_logs_services.foo"].Primary.Attributes
+		awsLogsServicesCount, _ := strconv.Atoi(resourceAttributes["aws_logs_services.#"])
+
+		if awsLogsServicesCount != len(awsLogsServices) {
+			return fmt.Errorf("expected %d aws logs services, got %d aws logs services",
+				awsLogsServicesCount, len(awsLogsServices))
+		}
+
+		return nil
+	}
+}
