15e0467ee592e5404619e3cecb3661412a1ccf21
Kevin Zou <kevin.zou@datadoghq.com>
2023-01-10 12:11:12 -0500

    update cassettes and test case

--- datadog/tests/resource_datadog_dashboard_style_test.go
+++ datadog/tests/resource_datadog_dashboard_style_test.go
@@ -7,11 +7,16 @@ resource "datadog_dashboard" "style_dashboard" {
 	title         = "{{uniq}}"
 	description   = "Created using the Datadog provider in Terraform"
 	layout_type   = "ordered"
-	is_read_only  = "true"
 
 	widget {
 		timeseries_definition {
 			request {
+				query {
+					metric_query {
+						query       = "avg:system.cpu.user{app:general} by {env}"
+						name        = "query"
+					}
+				}		  
 				formula {
 					formula_expression = "query"
 					style {
@@ -26,14 +31,12 @@ resource "datadog_dashboard" "style_dashboard" {
 `
 
 var datadogDashboardStyleAsserts = []string{
-	"widget.0.service_level_objective_definition.0.title_size = 16",
-	"is_read_only = true",
 	"title = {{uniq}}",
 	"description = Created using the Datadog provider in Terraform",
 	"layout_type = ordered",
-	"widget.0.timeseries_definition.request.formula.formula_expression = query",
-	"widget.0.timeseries_definition.request.formula.style.palette = warm",
-	"widget.0.timeseries_definition.request.formula.style.palette_index = 2",
+	"widget.0.timeseries_definition.0.request.0.formula.0.formula_expression = query",
+	"widget.0.timeseries_definition.0.request.0.formula.0.style.0.palette = warm",
+	"widget.0.timeseries_definition.0.request.0.formula.0.style.0.palette_index = 2",
 }
 
 func TestAccDatadogDashboardStyle(t *testing.T) {
