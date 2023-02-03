b0dc0bfe9dd6eb024d20bf325016de1ef27ca691
Carl Martensen <carl.martensen@datadoghq.com>
2023-02-03 12:40:45 -0800

    [datadog_monitor_config_policy] Add monitor config policies resource and data source (#1750)
    * Bump go client
    
    * Fix service account migration
    
    * Add monitor config policies resource and data source
    
    * Remove example for discarded data source
    
    * Autogen the docs
    
    * Fix description
    
    * Update doc
    
    * Update cassette
    
    * Uncomment test assertions
    
    * Update cassette
    
    * Remove test depends_on and see if that fixes CI test failure
    
    * Revert "Remove test depends_on and see if that fixes CI test failure"
    
    This reverts commit 6ef4a46f6d14e3ef7561fcd0afa669711bc6322b.
    
    * Remove flaky and unnecessary destroy check
    
    * Update cassette

---
+++ datadog/tests/data_source_datadog_monitor_config_policies_test.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDatadogMonitorConfigPoliciesDatasource(t *testing.T) {
+	_, accProviders := testAccProviders(context.Background(), t)
+	datasource := "data.datadog_monitor_config_policies.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: accProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceMonitorConfigPolicies(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(datasource, "id", "monitor-config-policies"),
+					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.#", "2"),
+					resource.TestCheckResourceAttrSet(datasource, "monitor_config_policies.0.id"),
+					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.tag_key", "tagKey1"),
+					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.tag_key_required", "false"),
+					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.valid_tag_values.#", "1"),
+					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.valid_tag_values.0", "value"),
+
+					resource.TestCheckResourceAttrSet(datasource, "monitor_config_policies.1.id"),
+					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.1.tag_policy.0.tag_key", "tagKey2"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourceMonitorConfigPolicies() string {
+	return fmt.Sprintf(`
+	data "datadog_monitor_config_policies" "test" {
+		depends_on   = ["datadog_monitor_config_policy.test1", "datadog_monitor_config_policy.test2"]
+	} 
+    %s 
+    %s`,
+		testAccCheckDatadogMonitorConfigPolicyConfig("test1", "tagKey1"),
+		testAccCheckDatadogMonitorConfigPolicyConfig("test2", "tagKey2"))
+}
