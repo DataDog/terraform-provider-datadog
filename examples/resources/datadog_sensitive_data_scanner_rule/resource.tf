3db6859487950e2ea0e9aa8b1284da1899c5f0c5
skarimo <40482491+skarimo@users.noreply.github.com>
2023-03-02 13:31:47 -0500

    [datadog_sensitive_data_scanner_rule] Create Sensitive Data Scanner rule resources (#1738)
    * ✨ Add Standard Pattern Data Source
    
    * ✨ Add Scanning Rule Resource
    
    Co-authored-by: Arthur Belleville <arthur.belleville@datadoghq.com>
    Co-authored-by: Thomas Hervé <thomas.herve@datadoghq.com>

---
+++ examples/resources/datadog_sensitive_data_scanner_rule/resource.tf
@@ -0,0 +1,41 @@
+# Create new sensitive_data_scanner_rule resource in a sensitive_data_scanner_group
+
+resource "datadog_sensitive_data_scanner_group" "mygroup" {
+  name        = "My new scanning group"
+  description = "A relevant description"
+  filter {
+    query = "service:my-service"
+  }
+  is_enabled   = true
+  product_list = ["apm"]
+}
+
+resource "datadog_sensitive_data_scanner_rule" "myrule" {
+  name                = "My new rule"
+  description         = "Another description"
+  group_id            = datadog_sensitive_data_scanner_group.mygroup.id
+  excluded_namespaces = ["username"]
+  is_enabled          = true
+  pattern             = "myregex"
+  tags                = ["sensitive_data:true"]
+  text_replacement {
+    number_of_chars    = 0
+    replacement_string = ""
+    type               = "hash"
+  }
+}
+
+data "datadog_sensitive_data_scanner_standard_pattern" "aws_sp" {
+  filter = "AWS Access Key ID Scanner"
+}
+
+resource "datadog_sensitive_data_scanner_rule" "mylibraryrule" {
+  name        = "My library rule"
+  description = "A description"
+  group_id    = datadog_sensitive_data_scanner_group.mygroup.id
+  // As standard_pattern_id is provided, the resource MUST NOT contain the "pattern" attribute
+  standard_pattern_id = data.datadog_sensitive_data_scanner_standard_pattern.aws_sp.id
+  excluded_namespaces = ["username"]
+  is_enabled          = true
+  tags                = ["sensitive_data:true"]
+}
