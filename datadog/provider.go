d0b42a99fa7bf2bca159b3d227fba7f2ae83cfd6
Kevin Zou <kevin.zou@datadoghq.com>
2023-04-10 13:22:30 -0400

    framework upgrade for SDS group order

--- datadog/provider.go
+++ datadog/provider.go
@@ -192,7 +192,6 @@ func Provider() *schema.Provider {
 			"datadog_security_monitoring_default_rule":     resourceDatadogSecurityMonitoringDefaultRule(),
 			"datadog_security_monitoring_rule":             resourceDatadogSecurityMonitoringRule(),
 			"datadog_security_monitoring_filter":           resourceDatadogSecurityMonitoringFilter(),
-			"datadog_sensitive_data_scanner_group_order":   resourceDatadogSDSGroupOrder(),
 			"datadog_sensitive_data_scanner_group":         resourceDatadogSensitiveDataScannerGroup(),
 			"datadog_sensitive_data_scanner_rule":          resourceDatadogSensitiveDataScannerRule(),
 			"datadog_service_level_objective":              resourceDatadogServiceLevelObjective(),
