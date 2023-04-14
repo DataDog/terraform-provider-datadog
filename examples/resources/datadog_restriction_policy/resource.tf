4e92fbabde05673070d4add4aa267f139a836922
Agnes Jang <agnes.jang@datadoghq.com>
2023-04-14 14:35:51 -0400

    formatting

--- examples/resources/datadog_restriction_policy/resource.tf
+++ examples/resources/datadog_restriction_policy/resource.tf
@@ -3,8 +3,8 @@
 
 
 resource "datadog_restriction_policy" "foo" {
-    bindings {
+  bindings {
     principals = ["role:00000000-0000-1111-0000-000000000000"]
-    relation = "editor"
-    }
+    relation   = "editor"
+  }
 }
