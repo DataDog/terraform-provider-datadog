09e660d25b602ac91c15680626f9f63f074df06f
Vitor Correa <vitor.correa@datadoghq.com>
2023-04-19 15:57:10 +0200

    Add spans metrics support

--- datadog/tests/provider_test.go
+++ datadog/tests/provider_test.go
@@ -161,6 +161,7 @@ var testFiles2EndpointTags = map[string]string{
 	"tests/resource_datadog_service_level_objective_test":                    "service-level-objectives",
 	"tests/resource_datadog_service_definition_yaml_test":                    "service-definition",
 	"tests/resource_datadog_slo_correction_test":                             "slo_correction",
+	"tests/resource_datadog_spans_metric_test":                               "spans-metric",
 	"tests/resource_datadog_synthetics_test_test":                            "synthetics",
 	"tests/resource_datadog_synthetics_global_variable_test":                 "synthetics",
 	"tests/resource_datadog_synthetics_private_location_test":                "synthetics",
