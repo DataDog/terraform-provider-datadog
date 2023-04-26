65ebcb87dbbc9d06eab7db4e7a8e57e569ad8d66
Kevin Zou <17015060+nkzou@users.noreply.github.com>
2023-04-26 10:02:53 -0400

    [datadog_synthetics_test] Remove required from jsonpath and xpath targetvalue fields (#1880)
    * remove required from targetvalue fields
    
    * set them optional and make docs

--- datadog/resource_datadog_synthetics_test_.go
+++ datadog/resource_datadog_synthetics_test_.go
@@ -458,7 +458,7 @@ func syntheticsAPIAssertion() *schema.Schema {
 							"targetvalue": {
 								Description: "Expected matching value.",
 								Type:        schema.TypeString,
-								Required:    true,
+								Optional:    true,
 							},
 						},
 					},
@@ -483,7 +483,7 @@ func syntheticsAPIAssertion() *schema.Schema {
 							"targetvalue": {
 								Description: "Expected matching value.",
 								Type:        schema.TypeString,
-								Required:    true,
+								Optional:    true,
 							},
 						},
 					},
