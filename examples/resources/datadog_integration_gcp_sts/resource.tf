5a361819b9d5b95110fdbe3887c97dd43baba8f9
Josh Huie <josh.huie@datadoghq.com>
2023-05-31 10:24:26 -0400

    update docs

--- examples/resources/datadog_integration_gcp_sts/resource.tf
+++ examples/resources/datadog_integration_gcp_sts/resource.tf
@@ -2,15 +2,16 @@
 
 // Service account should have compute.viewer, monitoring.viewer, and cloudasset.viewer roles.
 resource "google_service_account" "datadog_integration" {
-  account_id   = "datadog_integration"
+  account_id   = "datadogintegration"
   display_name = "Datadog Integration"
+  project = "gcp-project"
 }
 
 // Grant token creator role to the Datadog principal account.
 resource "google_service_account_iam_member" "sa_iam" {
   service_account_id = google_service_account.datadog_integration.name
   role               = "roles/iam.serviceAccountTokenCreator"
-  member             = format("serviceAccount:%s", datadog_integration_gcp_sts.integration_implementation.delegate_account_email)
+  member             = format("serviceAccount:%s", datadog_integration_gcp_sts.foo.delegate_account_email)
 }
 
 resource "datadog_integration_gcp_sts" "foo" {
