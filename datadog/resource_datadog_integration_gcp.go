353edd39863a1227392ba0eb5edec3b4ca184fe4
Josh Huie <josh.huie@datadoghq.com>
2023-06-13 13:31:31 -0400

    update documentation

--- datadog/resource_datadog_integration_gcp.go
+++ datadog/resource_datadog_integration_gcp.go
@@ -16,8 +16,8 @@ var integrationGcpMutex = sync.Mutex{}
 
 func resourceDatadogIntegrationGcp() *schema.Resource {
 	return &schema.Resource{
-		Description:        "Provides a Datadog - Google Cloud Platform integration resource. This can be used to create and manage Datadog - Google Cloud Platform integration.",
-		DeprecationMessage: "This resource is deprecated. Instead use the datadog_integration_gcp_sts resource.",
+		Description:        "This resource is deprecated — use the gcp_sts resource instead. Provides a Datadog - Google Cloud Platform integration resource. This can be used to create and manage Datadog - Google Cloud Platform integration.",
+		DeprecationMessage: "This resource is deprecated — use the datadog_integration_gcp_sts resource instead.",
 		CreateContext:      resourceDatadogIntegrationGcpCreate,
 		ReadContext:        resourceDatadogIntegrationGcpRead,
 		UpdateContext:      resourceDatadogIntegrationGcpUpdate,
