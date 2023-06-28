2597a8485978aabd854a6033268d60bd07f67947
Agnes Jang <agnes.jang@datadoghq.com>
2023-06-28 14:04:35 -0400

    formatting

--- examples/resources/datadog_restriction_policy/resource.tf
+++ examples/resources/datadog_restriction_policy/resource.tf
@@ -3,7 +3,7 @@
 
 
 resource "datadog_restriction_policy" "foo" {
-  resource_id  = "security-rule:abc-def-ghi"
+  resource_id = "security-rule:abc-def-ghi"
   bindings {
     principals = ["role:00000000-0000-1111-0000-000000000000"]
     relation   = "editor"
