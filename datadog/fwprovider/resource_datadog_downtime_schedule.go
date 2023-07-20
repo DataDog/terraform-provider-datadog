85ad7254e2eab6cb111ea4b7e45e3adf32364800
carl.martensen <carl.martensen@datadoghq.com>
2023-07-20 10:47:35 -0700

    Working, maybe

--- datadog/fwprovider/resource_datadog_downtime_schedule.go
+++ datadog/fwprovider/resource_datadog_downtime_schedule.go
@@ -168,8 +168,8 @@ func (r *DowntimeScheduleResource) Schema(_ context.Context, _ resource.SchemaRe
 									Description: "The `RRULE` standard for defining recurring events. For example, to have a recurring event on the first day of each month, set the type to `rrule` and set the `FREQ` to `MONTHLY` and `BYMONTHDAY` to `1`. Most common `rrule` options from the [iCalendar Spec](https://tools.ietf.org/html/rfc5545) are supported.  **Note**: Attributes specifying the duration in `RRULE` are not supported (for example, `DTSTART`, `DTEND`, `DURATION`). More examples available in this [downtime guide](https://docs.datadoghq.com/monitors/guide/suppress-alert-with-downtimes/?tab=api).",
 								},
 								"start": schema.StringAttribute{
-									Optional: true,
-									// Computed:    true,
+									Optional:    true,
+									Computed:    true,
 									Description: "ISO-8601 Datetime to start the downtime. Must not include a UTC offset. If not provided, the downtime starts the moment it is created.",
 								},
 							},
