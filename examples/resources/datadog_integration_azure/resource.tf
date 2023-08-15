d6c23ffdb59a29364fc3fc316b68330850afa9a1
Ava Silver <ava.silver@datadoghq.com>
2023-08-15 09:32:44 -0400

    changed tag

--- examples/resources/datadog_integration_azure/resource.tf
+++ examples/resources/datadog_integration_azure/resource.tf
@@ -4,7 +4,7 @@ resource "datadog_integration_azure" "sandbox" {
   client_id                 = "<azure_client_id>"
   client_secret             = "<azure_client_secret_key>"
   host_filters              = "examplefilter:true,example:true"
-  app_service_plan_filters  = "examplefilter:true,example:true"
+  app_service_plan_filters  = "examplefilter:true,example:another"
   automute                  = true
   cspm_enabled              = true
   custom_metrics_enabled    = false
