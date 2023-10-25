74fcc493549889ec12697c7c52d67fd636529a7f
Kevin Zou <kevin.zou@datadoghq.com>
2023-10-25 14:09:10 -0400

    properly find tag keys in the list

--- datadog/tests/data_source_datadog_monitor_config_policies_test.go
+++ datadog/tests/data_source_datadog_monitor_config_policies_test.go
@@ -2,9 +2,12 @@ package test
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestAccDatadogMonitorConfigPoliciesDatasource(t *testing.T) {
@@ -19,15 +22,8 @@ func TestAccDatadogMonitorConfigPoliciesDatasource(t *testing.T) {
 				Config: testAccDatasourceMonitorConfigPolicies(testName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(datasource, "id", "monitor-config-policies"),
-					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.#", "2"),
-					resource.TestCheckResourceAttrSet(datasource, "monitor_config_policies.0.id"),
-					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.tag_key", "tagKey1"),
-					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.tag_key_required", "false"),
-					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.valid_tag_values.#", "1"),
-					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.0.tag_policy.0.valid_tag_values.0", "value"),
-
-					resource.TestCheckResourceAttrSet(datasource, "monitor_config_policies.1.id"),
-					resource.TestCheckResourceAttr(datasource, "monitor_config_policies.1.tag_policy.0.tag_key", "tagKey2"),
+					testAccCheckDatadogMonitorConfigPolicyResourceExists(testName+"-1", "false", "value"),
+					testAccCheckDatadogMonitorConfigPolicyResourceExists(testName+"-2", "false", "value"),
 				),
 			},
 		},
@@ -35,5 +31,45 @@ func TestAccDatadogMonitorConfigPoliciesDatasource(t *testing.T) {
 }
 
 func testAccDatasourceMonitorConfigPolicies(name string) string {
-	return `data "datadog_monitor_config_policies" "test" {}`
+	return fmt.Sprintf(`
+	data "datadog_monitor_config_policies" "test" {
+		depends_on   = ["datadog_monitor_config_policy.%[1]s-2"]
+	} 
+    resource "datadog_monitor_config_policy" "%[1]s-1" {
+		policy_type = "tag"
+		tag_policy {
+			tag_key          = "%[1]s-1"
+			tag_key_required = false
+			valid_tag_values = ["value"]
+		}
+	}
+    resource "datadog_monitor_config_policy" "%[1]s-2" {
+		depends_on   = ["datadog_monitor_config_policy.%[1]s-1"]
+		policy_type = "tag"
+		tag_policy {
+			tag_key          = "%[1]s-2"
+			tag_key_required = false
+			valid_tag_values = ["value"]
+		}
+	}`, name)
+}
+
+func testAccCheckDatadogMonitorConfigPolicyResourceExists(tag_key string, tag_key_required string, tag_value string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, r := range s.RootModule().Resources {
+			if r.Type != "datadog_monitor_config_policies" {
+				continue
+			}
+			policy_num, _ := strconv.Atoi(r.Primary.Attributes["monitor_config_policies.#"])
+			for i := 0; i < policy_num; i++ {
+				tag_key_state := r.Primary.Attributes[fmt.Sprintf("monitor_config_policies.%d.tag_policy.0.tag_key", i)]
+				tag_key_required_state := r.Primary.Attributes[fmt.Sprintf("monitor_config_policies.%d.tag_policy.0.tag_key_required", i)]
+				tag_value_state := r.Primary.Attributes[fmt.Sprintf("monitor_config_policies.%d.tag_policy.0.valid_tag_values.0", i)]
+				if tag_key_state == tag_key && tag_key_required_state == tag_key_required && tag_value_state == tag_value {
+					return nil
+				}
+			}
+		}
+		return fmt.Errorf("missing monitor config policy")
+	}
 }
