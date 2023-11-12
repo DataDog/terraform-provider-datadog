0f8296a81ed7fc2651750d30eb9ea1db3dab29be
Salah Eddine Bachir-Cherif <salah.bachircherif@datadoghq.com>
2023-11-12 16:35:56 +0100

    fix wording

--- datadog/fwprovider/data_source_datadog_apm_retention_filters_order.go
+++ datadog/fwprovider/data_source_datadog_apm_retention_filters_order.go
@@ -39,7 +39,7 @@ func (d *datadogApmRetentionFiltersOrderDataSource) Schema(_ context.Context, _ 
 		Description: "Provides a Datadog [APM Retention Filters API](https://docs.datadoghq.com/api/v2/apm-retention-filters/) order datasource. This can be used to retrieve APM retention filters order.",
 		Attributes: map[string]schema.Attribute{
 			"filter_ids": schema.ListAttribute{
-				Description: "The filter IDs list. The order of filters IDs in this attribute defines the overall APM retention filters order.. If `filter_ids` is empty or not specified, it will import the actual order, and create the resource. Otherwise, it will try to update the order.",
+				Description: "The filter IDs list. The order of filters IDs in this attribute defines the overall APM retention filters order.",
 				ElementType: types.StringType,
 				Optional:    true,
 			},
