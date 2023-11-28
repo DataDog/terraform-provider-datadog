a45ca2f2d4f5b9656b0a4db86cf5f9005ea98718
Dasha Shifrina <dasha.shifrina@datadoghq.com>
2023-11-28 12:20:36 -0500

    timeseries test coverage

--- datadog/tests/provider_test.go
+++ datadog/tests/provider_test.go
@@ -157,6 +157,7 @@ var testFiles2EndpointTags = map[string]string{
 	"tests/resource_datadog_powerpack_scatterplot_test":                      "powerpacks",
 	"tests/resource_datadog_powerpack_servicemap_test":                       "powerpacks",
 	"tests/resource_datadog_powerpack_slo_test":                              "powerpacks",
+	"tests/resource_datadog_powerpack_timeseries_test":                       "powerpacks",
 	"tests/resource_datadog_powerpack_toplist_test":                          "powerpacks",
 	"tests/resource_datadog_powerpack_topology_map_test":                     "powerpacks",
 	"tests/resource_datadog_powerpack_trace_service_test":                    "powerpacks",
