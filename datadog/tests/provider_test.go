13ac2185c6748ab0eb1443dcaa4c08a2f29cae03
Dasha Shifrina <dasha.shifrina@datadoghq.com>
2023-11-28 12:40:58 -0500

    sunburst test coverage

--- datadog/tests/provider_test.go
+++ datadog/tests/provider_test.go
@@ -159,6 +159,7 @@ var testFiles2EndpointTags = map[string]string{
 	"tests/resource_datadog_powerpack_scatterplot_test":                      "powerpacks",
 	"tests/resource_datadog_powerpack_servicemap_test":                       "powerpacks",
 	"tests/resource_datadog_powerpack_slo_test":                              "powerpacks",
+	"tests/resource_datadog_powerpack_sunburst_test":                         "powerpacks",
 	"tests/resource_datadog_powerpack_timeseries_test":                       "powerpacks",
 	"tests/resource_datadog_powerpack_toplist_test":                          "powerpacks",
 	"tests/resource_datadog_powerpack_topology_map_test":                     "powerpacks",
