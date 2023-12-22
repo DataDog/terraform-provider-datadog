a5d617354b656c872a87b5c425011f385129c70a
Katie McKew <katie.mckew@datadoghq.com>
2023-12-22 13:59:55 -0500

    Docs

--- examples/resources/datadog_integration_aws_event_bridge/import.sh
+++ examples/resources/datadog_integration_aws_event_bridge/import.sh
@@ -1,1 +1,2 @@
-terraform import datadog_integration_aws_event_bridge.new_list "UPDATE_ME"
+# Amazon Web Service EventBridge integrations are imported using the Event Source name as listed for an integrated AWS account in Datadog
+terraform import datadog_integration_aws_event_bridge.foo event-source-name-abc12345
