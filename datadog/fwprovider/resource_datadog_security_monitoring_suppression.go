051cf9aee5c72ba4d6e95ddc851fbbc28daebff6
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2024-04-24 15:23:54 +0200

    [datadog_security_monitoring_suppression] Fix error handling when updating suppression payload

--- datadog/fwprovider/resource_datadog_security_monitoring_suppression.go
+++ datadog/fwprovider/resource_datadog_security_monitoring_suppression.go
@@ -154,6 +154,7 @@ func (r *securityMonitoringSuppressionResource) Update(ctx context.Context, requ
 
 	if err != nil {
 		response.Diagnostics.AddError("error while parsing resource", err.Error())
+		return
 	}
 
 	res, _, err := r.api.UpdateSecurityMonitoringSuppression(r.auth, state.Id.ValueString(), *suppressionPayload)
