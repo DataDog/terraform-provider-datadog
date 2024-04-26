c22b9cacf076b5fbadbba1ed97cb499277f517ac
Willson Wang <willson.wang@datadoghq.com>
2024-04-26 13:08:27 -0400

    remove some unintended changes

--- datadog/resource_datadog_authn_mapping.go
+++ datadog/resource_datadog_authn_mapping.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/internal/utils"
 
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
