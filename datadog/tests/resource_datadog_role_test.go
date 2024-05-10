8369e9c141545925f11869811c2513cd935e60ee
Kevin Zou <kevin.zou@datadoghq.com>
2024-05-10 14:50:33 -0400

    skip update-related tests to match api-spec

--- datadog/tests/resource_datadog_role_test.go
+++ datadog/tests/resource_datadog_role_test.go
@@ -42,23 +42,23 @@ func TestAccDatadogRole_CreateUpdate(t *testing.T) {
 					),
 				),
 			},
-			{
-				Config: testAccCheckDatadogRoleConfigUpdated(rolename),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDatadogRoleExists(accProvider, "datadog_role.foo"),
-					resource.TestCheckResourceAttr("datadog_role.foo", "name", rolename),
-					testCheckRolePermission(
-						"datadog_role.foo",
-						"data.datadog_permissions.foo",
-						"permissions.logs_read_index_data",
-					),
-					testCheckRolePermission(
-						"datadog_role.foo",
-						"data.datadog_permissions.foo",
-						"permissions.standard",
-					),
-				),
-			},
+			// {
+			// 	Config: testAccCheckDatadogRoleConfigUpdated(rolename),
+			// 	Check: resource.ComposeTestCheckFunc(
+			// 		testAccCheckDatadogRoleExists(accProvider, "datadog_role.foo"),
+			// 		resource.TestCheckResourceAttr("datadog_role.foo", "name", rolename),
+			// 		testCheckRolePermission(
+			// 			"datadog_role.foo",
+			// 			"data.datadog_permissions.foo",
+			// 			"permissions.logs_read_index_data",
+			// 		),
+			// 		testCheckRolePermission(
+			// 			"datadog_role.foo",
+			// 			"data.datadog_permissions.foo",
+			// 			"permissions.standard",
+			// 		),
+			// 	),
+			// },
 			{
 				Config: testAccCheckDatadogRoleConfigNoPerm(rolename),
 				Check: resource.ComposeTestCheckFunc(
@@ -176,7 +176,7 @@ func testAccCheckDatadogRoleConfigNoPerm(uniq string) string {
 data "datadog_permissions" foo {}
 
 resource "datadog_role" "foo" {
-  name      = "%snoperm"
+  name      = "%s"
 }`, uniq)
 }
 
