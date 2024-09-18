8ea5c87443b0cdacfa5dfe909fa3369e6b4c102a
Romain Guarinoni <romain.guarinoni@datadoghq.com>
2024-09-18 17:08:56 +0200

    [datadog_synthetics_test] Add javascript assertion to Synthetics Assertion (#2576)
    * feat: update code to add js assertion
    
    * feat: update tests and record cassettes
    
    * feat: add docs

--- datadog/tests/resource_datadog_synthetics_test_test.go
+++ datadog/tests/resource_datadog_synthetics_test_test.go
@@ -944,7 +944,7 @@ func createSyntheticsAPITestStep(ctx context.Context, accProvider func() (*schem
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.foo", "variables_from_script", "dd.variable.set('FOO', 'hello');"),
 			resource.TestCheckResourceAttr(
-				"datadog_synthetics_test.foo", "assertion.#", "5"),
+				"datadog_synthetics_test.foo", "assertion.#", "6"),
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.foo", "assertion.0.type", "header"),
 			resource.TestCheckResourceAttr(
@@ -973,6 +973,10 @@ func createSyntheticsAPITestStep(ctx context.Context, accProvider func() (*schem
 				"datadog_synthetics_test.foo", "assertion.3.operator", "doesNotContain"),
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.foo", "assertion.3.target", "terraform"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_test.foo", "assertion.5.type", "javascript"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_test.foo", "assertion.5.code", "const hello = 'world';"),
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.foo", "locations.#", "1"),
 			resource.TestCheckTypeSetElemAttr(
@@ -1100,6 +1104,10 @@ resource "datadog_synthetics_test" "foo" {
 		type = "bodyHash"
 		operator = "md5"
 		target = "a"
+	}
+	assertion {
+		type = "javascript"
+		code = "const hello = 'world';"
 	}
 	locations = [ "aws:eu-central-1" ]
 
@@ -1352,7 +1360,7 @@ func createSyntheticsAPITestStepNewAssertionsOptions(ctx context.Context, accPro
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.bar", "request_client_certificate.0.key.0.filename", "key"),
 			resource.TestCheckResourceAttr(
-				"datadog_synthetics_test.bar", "assertion.#", "11"),
+				"datadog_synthetics_test.bar", "assertion.#", "12"),
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.bar", "assertion.0.type", "header"),
 			resource.TestCheckResourceAttr(
@@ -1453,6 +1461,10 @@ func createSyntheticsAPITestStepNewAssertionsOptions(ctx context.Context, accPro
 				"datadog_synthetics_test.bar", "assertion.10.targetjsonpath.0.jsonpath", "$.myKey"),
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.bar", "assertion.10.targetjsonpath.0.operator", "isUndefined"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_test.bar", "assertion.11.type", "javascript"),
+			resource.TestCheckResourceAttr(
+				"datadog_synthetics_test.bar", "assertion.11.code", "const hello = 'world';"),
 			resource.TestCheckResourceAttr(
 				"datadog_synthetics_test.bar", "locations.#", "1"),
 			resource.TestCheckResourceAttr(
@@ -1616,6 +1628,10 @@ resource "datadog_synthetics_test" "bar" {
 			operator    = "isUndefined"
 		}
     }
+	assertion {
+		type = "javascript"
+		code = "const hello = 'world';"
+	}
 
 	locations = [ "aws:eu-central-1" ]
 	options_list {
