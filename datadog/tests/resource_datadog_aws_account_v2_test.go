45ad980400336d7f83b47c3d1be0c87ef4137825
Katie McKew <katie.mckew@datadoghq.com>
2024-11-19 15:29:18 -0500

    Add tests and upate example

--- datadog/tests/resource_datadog_aws_account_v2_test.go
+++ datadog/tests/resource_datadog_aws_account_v2_test.go
@@ -22,7 +22,7 @@ func TestAccAwsAccountV2Basic(t *testing.T) {
 		ProtoV5ProviderFactories: accProviders,
 		CheckDestroy:             testAccCheckDatadogAwsAccountV2Destroy(providers.frameworkProvider),
 		Steps: []resource.TestStep{
-			{
+			{ // create account
 				Config: testAccCheckDatadogAwsAccountV2(accountID, uniq),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckDatadogAwsAccountV2Exists(providers.frameworkProvider),
@@ -30,6 +30,63 @@ func TestAccAwsAccountV2Basic(t *testing.T) {
 						"datadog_aws_account_v2.foo", "aws_account_id", accountID),
 					resource.TestCheckResourceAttr(
 						"datadog_aws_account_v2.foo", "aws_partition", "aws"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "aws_regions.aws_regions_include_only.include_only.0", "us-east-1"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "auth_config.aws_auth_config_role.role_name", "test"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "logs_config.lambda_forwarder.sources.0", "s3"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "logs_config.lambda_forwarder.lambdas.0", "arn:aws:lambda:us-east-1:123456789123:function:test-fn"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.automute_enabled", "true"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.collect_cloudwatch_alarms", "true"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.collect_custom_metrics", "true"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.enabled", "true"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.namespace_filters.aws_namespace_filters_include_only.include_only.0", "AWS/EC2"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "resources_config.cloud_security_posture_management_collection", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "resources_config.extended_collection", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "traces_config.xray_services.x_ray_services_include_all.include_all", "true"),
+				),
+			}, { // update account
+				Config: testAccCheckDatadogAwsAccountV2UpdateConfig(accountID, uniq),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDatadogAwsAccountV2Exists(providers.frameworkProvider),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "aws_account_id", accountID),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "aws_partition", "aws"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "aws_regions.aws_regions_include_all.include_all", "true"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "auth_config.aws_auth_config_role.role_name", fmt.Sprintf("test-%s", uniq)),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "logs_config.lambda_forwarder.sources.#", "0"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "logs_config.lambda_forwarder.lambdas.#", "0"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.automute_enabled", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.collect_cloudwatch_alarms", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.collect_custom_metrics", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.enabled", "false"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "metrics_config.namespace_filters.aws_namespace_filters_include_only.exclude_only.#", "0"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "resources_config.cloud_security_posture_management_collection", "true"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "resources_config.extended_collection", "true"),
+					resource.TestCheckResourceAttr(
+						"datadog_aws_account_v2.foo", "traces_config.xray_services.x_ray_services_include_only.include_only.#", "0"),
 				),
 			},
 		},
@@ -37,51 +94,98 @@ func TestAccAwsAccountV2Basic(t *testing.T) {
 }
 
 func testAccCheckDatadogAwsAccountV2(accountID, uniq string) string {
-	// Update me to make use of the unique value
+	return fmt.Sprintf(`
+resource "datadog_aws_account_v2" "foo" {
+        aws_account_id = %s
+        account_tags = ["tag:%s"]
+        aws_partition = "aws"
+    aws_regions {
+        aws_regions_include_only {
+            include_only = ["us-east-1"]
+        }
+    }
+    auth_config {
+        aws_auth_config_role {
+            role_name = "test"
+        }
+    }
+    logs_config {
+        lambda_forwarder {
+            sources = ["s3", "lambda"]
+            lambdas = ["arn:aws:lambda:us-east-1:123456789123:function:test-fn"]
+        }
+    }
+        metrics_config {
+            automute_enabled = true
+            collect_cloudwatch_alarms = true
+            collect_custom_metrics = true
+            enabled = true
+        namespace_filters {
+        aws_namespace_filters_include_only {
+            include_only = ["AWS/EC2"]
+        }
+        }
+
+        }
+        resources_config {
+        cloud_security_posture_management_collection = false
+        extended_collection = false
+        }
+    traces_config {
+        xray_services {
+            x_ray_services_include_all {
+                include_all = true
+            }
+        }
+    }
+}`, accountID, uniq)
+}
+
+func testAccCheckDatadogAwsAccountV2UpdateConfig(accountID, uniq string) string {
 	return fmt.Sprintf(`
 resource "datadog_aws_account_v2" "foo" {
     aws_account_id = %s
-    account_tags = ["tag:%s"]
+    account_tags = []
     aws_partition = "aws"
-	aws_regions {
-		aws_regions_include_only {
-			include_only = ["us-east-1"]
-		}
-	}
-	auth_config {
-		aws_auth_config_role {
-			role_name = "test"
-		}
-	}
-	logs_config {
-		lambda_forwarder {
-			sources = ["s3", "lambda"]
-			lambdas = ["arn:aws:lambda:us-east-1:123456789123:function:test-fn"]
-		}
-	}
-    metrics_config {
-  	  automute_enabled = true
-  	  collect_cloudwatch_alarms = true
-  	  collect_custom_metrics = true
-  	  enabled = true
-	  namespace_filters {
-		aws_namespace_filters_include_only {
-			include_only = ["AWS/EC2"]
-		}
-	  }
-
-  	}
-    resources_config {
-		cloud_security_posture_management_collection = false
-		extended_collection = true
+  aws_regions {
+    aws_regions_include_all {
+      include_all = true
     }
-	traces_config {
-		xray_services {
-			x_ray_services_include_all {
-				include_all = true
-			}
-		}
-	}
+  }
+  auth_config {
+    aws_auth_config_role {
+      role_name = "test-%s"
+    }
+  }
+  logs_config {
+    lambda_forwarder {
+      sources = []
+      lambdas = []
+    }
+  }
+    metrics_config {
+      automute_enabled = false
+      collect_cloudwatch_alarms = false
+      collect_custom_metrics = false
+      enabled = false
+    namespace_filters {
+    aws_namespace_filters_exclude_only {
+      exclude_only = []
+    }
+    }
+
+    }
+    resources_config {
+    cloud_security_posture_management_collection = true
+    extended_collection = true
+    }
+  traces_config {
+    xray_services {
+      x_ray_services_include_only {
+        include_only = []
+      }
+    }
+  }
 }`, accountID, uniq)
 }
 
--- examples/resources/datadog_aws_account_v2/resource.tf
+++ examples/resources/datadog_aws_account_v2/resource.tf
@@ -1,29 +1,39 @@
 # Create new aws_account_v2 resource
 
 resource "datadog_aws_account_v2" "foo" {
-  account_tags   = "UPDATE ME"
+  account_tags   = ["env:prod"]
   aws_account_id = "123456789012"
   aws_partition  = "aws"
   logs_config {
     lambda_forwarder {
-      lambdas = "UPDATE ME"
-      sources = "UPDATE ME"
+      lambdas = ["arn:aws:lambda:us-east-1:123456789012:function:my-lambda"]
+      sources = ["s3"]
     }
   }
   metrics_config {
-    automute_enabled          = True
-    collect_cloudwatch_alarms = True
-    collect_custom_metrics    = True
-    enabled                   = True
+    automute_enabled          = true
+    collect_cloudwatch_alarms = true
+    collect_custom_metrics    = true
+    enabled                   = true
+    namespace_filters {
+      aws_namespace_filters_exclude_only {
+        exclude_only = ["AWS/SQS", "AWS/ElasticMapReduce"]
+      }
+    }
     tag_filters {
       namespace = "AWS/EC2"
-      tags      = "UPDATE ME"
+      tags      = ["datadog:true"]
     }
   }
   resources_config {
-    cloud_security_posture_management_collection = "UPDATE ME"
-    extended_collection                          = "UPDATE ME"
+    cloud_security_posture_management_collection = false
+    extended_collection                          = true
   }
   traces_config {
+    xray_services {
+      x_ray_services_include_all {
+        include_all = true
+      }
+    }
   }
 }
