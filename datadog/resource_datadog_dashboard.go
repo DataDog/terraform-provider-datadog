fb93ef8df8350eaf39893b3477c2a568fd8a2115
Kevin Zou <kevin.zou@datadoghq.com>
2024-12-16 16:25:20 -0500

    make the wording of the deprecation stronger

--- datadog/resource_datadog_dashboard.go
+++ datadog/resource_datadog_dashboard.go
@@ -136,7 +136,7 @@ func resourceDatadogDashboard() *schema.Resource {
 					Default:       false,
 					ConflictsWith: []string{"restricted_roles"},
 					Description:   "Whether this dashboard is read-only.",
-					Deprecated:    "Prefer using `restricted_roles` to define which roles are required to edit the dashboard.",
+					Deprecated:    "This field is deprecated and non-functional. Use `restricted_roles` instead to define which roles are required to edit the dashboard.",
 				},
 				"tags": {
 					Type:        schema.TypeList,
