e84279ed04f71844a44ff94969a7549f986b415f
Grant Palmer <grant.palmer@datadoghq.com>
2025-01-14 20:33:46 -0500

    sort imports

--- datadog/fwprovider/resource_datadog_azure_integration.go
+++ datadog/fwprovider/resource_datadog_azure_integration.go
@@ -3,7 +3,6 @@ package fwprovider
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listdefault"
 	"sync"
 
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
@@ -13,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
