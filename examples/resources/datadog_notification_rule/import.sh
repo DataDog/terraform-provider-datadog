82e2d1ded3b1ffea3c9dcddcb05c4ec6a44863b4
Fred Moster <frederic.moster@datadoghq.com>
2025-02-07 15:44:20 +0100

    Add example

---
+++ examples/resources/datadog_notification_rule/import.sh
@@ -0,0 +1,1 @@
+terraform import datadog_notification_rule.signal_rule yq9-t9l-bso
---
+++ examples/resources/datadog_notification_rule/resource.tf
@@ -0,0 +1,21 @@
+resource "datadog_notification_rule" "signal_rule" {
+  name = "My signal notification rule"
+  selectors {
+    trigger_source = "security_signals"
+    rule_types     = ["workload_security"]
+    query          = "env:prod"
+  }
+  enabled = false
+  targets = ["bob@email.com", "alice@email.com"]
+}
+
+resource "datadog_notification_rule" "vulnerability_rule" {
+  name = "My vulnerability notification rule"
+  selectors {
+    trigger_source = "security_findings"
+    rule_types     = ["application_library_vulnerability", "identity_risk"]
+    severities     = ["critical", "high"]
+  }
+  time_aggregation = 36000
+  targets          = ["john@email.com"]
+}
