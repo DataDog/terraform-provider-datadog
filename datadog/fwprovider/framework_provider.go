28c73da7f077a1376a69b4e9088eaa821d4821f2
Fred Moster <frederic.moster@datadoghq.com>
2025-02-12 14:45:07 +0100

    [resource_datadog_security_notification_rule] Add notification rule terraform provider (#2806)
    * Add notification rule terraform provider
    
    * Add cassettes
    
    * Add documentation
    
    * Add test
    
    * Integrate documentation feedback
    
    * Add example
    
    * Handle terraform import properly
    
    * Lint docs
    
    * Update cassettes
    
    * Rename resource
    
    * Use ResourceIDAttribute for resource id

--- datadog/fwprovider/framework_provider.go
+++ datadog/fwprovider/framework_provider.go
@@ -51,6 +51,7 @@ var Resources = []func() resource.Resource{
 	NewIntegrationGcpResource,
 	NewIntegrationGcpStsResource,
 	NewIpAllowListResource,
+	NewSecurityNotificationRuleResource,
 	NewRestrictionPolicyResource,
 	NewRumApplicationResource,
 	NewRumMetricResource,
