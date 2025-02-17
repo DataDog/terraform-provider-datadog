7f90720688d66edff92f930c92126d2a63000224
Etienne Carriere <etienne.carriere@datadoghq.com>
2025-02-17 16:24:11 +0100

    Add test

--- datadog/tests/resource_datadog_shared_dashboard_test.go
+++ datadog/tests/resource_datadog_shared_dashboard_test.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
-
-	"github.com/terraform-providers/terraform-provider-datadog/datadog"
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/fwprovider"
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/internal/utils"
 )
@@ -23,42 +21,58 @@ func TestAccSharedDashboardBasic(t *testing.T) {
 		CheckDestroy:             testAccCheckDatadogSharedDashboardDestroy(providers.frameworkProvider),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckDatadogSharedDashboard(uniq),
+				Config: testAccCheckDatadogSharedDashboardOpenDashboard(uniq),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckDatadogSharedDashboardExists(providers.frameworkProvider),
+					resource.TestCheckResourceAttr("datadog_shared_dashboard.shared_dashboard", "share_type", "open"),
+					resource.TestCheckResourceAttr("datadog_shared_dashboard.shared_dashboard", "dashboard_type", "custom_timeboard"),
+					checkRessourceAttributeRegex("datadog_shared_dashboard.shared_dashboard", "public_url", "https://p.datadoghq.com/.*"),
+					resource.TestCheckResourceAttrSet("datadog_shared_dashboard.shared_dashboard", "token"),
+					resource.TestCheckResourceAttrSet("datadog_shared_dashboard.shared_dashboard", "author_handle"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckDatadogSharedDashboard(uniq string) string {
+func testAccCheckDatadogSharedDashboardOpenDashboard(uniq string) string {
 	// Update me to make use of the unique value
-	return fmt.Sprintf(`resource "datadog_shared_dashboard" "foo" {
-    body {
-    author {
-    handle = "test@datadoghq.com"
-    name = "UPDATE ME"
+	return fmt.Sprintf(`resource "datadog_dashboard" "dash_two" {
+		title = "%s"
+		description  = "Created using the Datadog provider in Terraform"
+		layout_type  = "ordered"
+		template_variable {
+          name    = "datacenter"
+          prefix  = "datacenter"
+          default = "my-datacenter"
+        }
+
+		widget {
+			alert_graph_definition {
+				alert_id = "1234"
+				viz_type = "timeseries"
+				title = "Widget Title"
+				live_span = "1h"
+			}
+		}
+	}
+
+	resource "datadog_shared_dashboard" "shared_dashboard" {
+		dashboard_id = datadog_dashboard.dash_two.id
+		dashboard_type = "custom_timeboard"
+		global_time {
+			live_span = "1h"
+		}
+		global_time_selectable_enabled = true
+		selectable_template_vars {
+		  default_value = "*"
+		  name          = "datacenter"
+		  prefix        = "datacenter"
+		  visible_tags  = ["value1", "value2", "value3"]
+		}
+		share_type = "open"
     }
-    created_at = "UPDATE ME"
-    dashboard_id = "123-abc-456"
-    dashboard_type = "custom_timeboard"
-    global_time {
-    live_span = "1h"
-    }
-    global_time_selectable_enabled = "UPDATE ME"
-    public_url = "UPDATE ME"
-    selectable_template_vars {
-    default_value = "UPDATE ME"
-    name = "UPDATE ME"
-    prefix = "UPDATE ME"
-    visible_tags = "UPDATE ME"
-    }
-    share_list = ["test@datadoghq.com", "test2@email.com"]
-    share_type = "UPDATE ME"
-    token = "UPDATE ME"
-    }
-}`, uniq)
+`, uniq)
 }
 
 func testAccCheckDatadogSharedDashboardDestroy(accProvider *fwprovider.FrameworkProvider) func(*terraform.State) error {
