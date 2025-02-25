c684fd0388906a08ae61e1f653217a798fc3cab0
Etienne Carriere <etienne.carriere@datadoghq.com>
2025-02-25 07:40:57 +0100

    Add WarningCallout function

--- datadog/utils.go
+++ datadog/utils.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func DeprecatedDocumentation(description string, replacedResource *string) string {
 	if replacedResource != nil {
-		return fmt.Sprintf("!>This resource is deprecated - use the `%s` resource instead.\n\n%s", *replacedResource, description)
+		return fmt.Sprintf("%s%s", WarningCallout(fmt.Sprintf("This resource is deprecated - use the `%s` resource instead.", *replacedResource)), description)
 	} else {
-		return fmt.Sprintf("!>This resource is deprecated \n\n %s", description)
+		return fmt.Sprintf("%s%s", WarningCallout("This resource is deprecated"), description)
 	}
+}
+
+func WarningCallout(message string) string {
+	return fmt.Sprintf("!>%s\n\n", message)
 }
 
 func Ptr[T any](v T) *T {
