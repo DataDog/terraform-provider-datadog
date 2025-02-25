3cd9d519af465089182bbcdaaa811a46d425aa60
Etienne Carriere <etienne.carriere@datadoghq.com>
2025-02-25 09:46:39 +0100

    Fix code violation

--- datadog/utils.go
+++ datadog/utils.go
@@ -5,9 +5,8 @@ import "fmt"
 func DeprecatedDocumentation(description string, replacedResource *string) string {
 	if replacedResource != nil {
 		return fmt.Sprintf("%s%s", WarningCallout(fmt.Sprintf("This resource is deprecated - use the `%s` resource instead.", *replacedResource)), description)
-	} else {
-		return fmt.Sprintf("%s%s", WarningCallout("This resource is deprecated"), description)
 	}
+	return fmt.Sprintf("%s%s", WarningCallout("This resource is deprecated"), description)
 }
 
 func WarningCallout(message string) string {
