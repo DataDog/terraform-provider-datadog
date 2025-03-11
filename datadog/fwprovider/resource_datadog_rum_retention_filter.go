042e14928a58dffd19715ab3181b53986e56c174
Kwang Jun Kim <kwangjun.kim@datadoghq.com>
2025-03-11 15:26:14 -0400

    reformat code

--- datadog/fwprovider/resource_datadog_rum_retention_filter.go
+++ datadog/fwprovider/resource_datadog_rum_retention_filter.go
@@ -3,6 +3,8 @@ package fwprovider
 import (
 	"context"
 	"errors"
+	"strings"
+
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
@@ -13,7 +15,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"strings"
 
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/internal/utils"
 )
