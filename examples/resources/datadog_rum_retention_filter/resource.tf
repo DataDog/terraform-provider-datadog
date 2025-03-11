366b39a82506aedf2c0e475d67267e693a1ea8e8
Kwang Jun Kim <kwangjun.kim@datadoghq.com>
2025-03-11 15:26:14 -0400

    reformat code

--- examples/resources/datadog_rum_retention_filter/resource.tf
+++ examples/resources/datadog_rum_retention_filter/resource.tf
@@ -1,10 +1,10 @@
 # Create new rum_retention_filter resource
 
 resource "datadog_rum_retention_filter" "testing_rum_retention_filter" {
-  app_id = "a7f5b0a3-9f73-4fdb-8fe1-91a8fc5a7a72"
-  name = "testing.rum.retention_filter"
-  event_type = "session"
+  app_id      = "a7f5b0a3-9f73-4fdb-8fe1-91a8fc5a7a72"
+  name        = "testing.rum.retention_filter"
+  event_type  = "session"
   sample_rate = 41
-  query = "@session.has_replay:true"
-  enabled = false
+  query       = "@session.has_replay:true"
+  enabled     = false
 }
