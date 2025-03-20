ef34c7ea64e478d9bae343b2c029329e6181a4ca
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2025-03-20 10:24:07 -0400

    update import docs

--- examples/resources/datadog_integration_azure/import.sh
+++ examples/resources/datadog_integration_azure/import.sh
@@ -1,3 +1,3 @@
 # Microsoft Azure integrations can be imported using their `tenant name` and `client` id separated with a colon (`:`).
+# The client_secret should be passed by setting the environment variable CLIENT_SECRET
 terraform import datadog_integration_azure.sandbox ${tenant_name}:${client_id}
-
