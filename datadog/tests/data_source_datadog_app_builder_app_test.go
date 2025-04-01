8eda2cfd06a78b42bf6d9c89238789c982efe96f
Alexis Jacob <alexis.jacob@datadoghq.com>
2025-04-01 11:52:34 +0200

    [datadog_synthetics_test] Add an example for ICMP testing (#2939)
    * New example
    
    * add tags in the example

--- datadog/tests/data_source_datadog_app_builder_app_test.go
+++ datadog/tests/data_source_datadog_app_builder_app_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/fwprovider"
 )
 
--- datadog/tests/resource_datadog_app_builder_app_test.go
+++ datadog/tests/resource_datadog_app_builder_app_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/fwprovider"
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/internal/utils"
 )
