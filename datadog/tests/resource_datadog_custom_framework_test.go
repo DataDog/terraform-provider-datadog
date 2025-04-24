10203d385de18df0c16aba4bdae887f6890e68e1
Neha Konjeti <neha.konjeti@datadoghq.com>
2025-04-23 23:56:59 -0400

    passed set create test

--- datadog/tests/resource_datadog_custom_framework_test.go
+++ datadog/tests/resource_datadog_custom_framework_test.go
@@ -12,10 +12,10 @@ import (
 	"github.com/terraform-providers/terraform-provider-datadog/datadog/fwprovider"
 )
 
-func TestCustomFramework_bgasic(t *testing.T) {
+func TestCustomFramework_create(t *testing.T) {
 	t.Parallel()
 	handle := fmt.Sprintf("handle-%d", rand.Intn(100000))
-	version := "1.0"
+	version := fmt.Sprintf("version-%d", rand.Intn(100000))
 
 	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
 	path := "datadog_custom_framework.sample_rules"
@@ -25,24 +25,136 @@ func TestCustomFramework_bgasic(t *testing.T) {
 		CheckDestroy:             testAccCheckDatadogFrameworkDestroy(ctx, providers.frameworkProvider, path, version, handle),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckDatadogCreateFramework(handle, version),
+				Config: testAccCheckDatadogCreateFramework(version, handle),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(path, "handle", handle),
 					resource.TestCheckResourceAttr(path, "version", version),
-					resource.TestCheckResourceAttr(path, "name", "test-framework"),
+					resource.TestCheckResourceAttr(path, "name", "new-framework-terraform"),
 					resource.TestCheckResourceAttr(path, "description", "test description"),
 					resource.TestCheckResourceAttr(path, "icon_url", "test url"),
-					resource.TestCheckResourceAttr(path, "requirements.#", "1"),
-					resource.TestCheckResourceAttr(path, "requirements.0.name", "requirement1"),
-					resource.TestCheckResourceAttr(path, "requirements.0.controls.#", "1"),
-					resource.TestCheckResourceAttr(path, "requirements.0.controls.0.name", "control1"),
-					resource.TestCheckResourceAttr(path, "requirements.0.controls.0.rules_id.#", "1"),
-					resource.TestCheckResourceAttr(path, "requirements.0.controls.0.rules_id.0", "def-000-be9"),
-					resource.TestCheckResourceAttr(path, "requirements.0.controls.0.rules_id.#", "1"),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*", map[string]string{
+						"name": "requirement1",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*.controls.*", map[string]string{
+						"name": "control1",
+					}),
+					resource.TestCheckTypeSetElemAttr(path, "requirements.*.controls.*.rules_id.*", "def-000-be9"),
+				),
+			},
+			{
+				Config: testAccCheckDatadogCreateFrameworkWithMultipleRequirements(version, handle),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(path, "handle", handle),
+					resource.TestCheckResourceAttr(path, "version", version),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*", map[string]string{
+						"name": "compliance-requirement",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*", map[string]string{
+						"name": "security-requirement",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*.controls.*", map[string]string{
+						"name": "compliance-control",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*.controls.*", map[string]string{
+						"name": "security-control",
+					}),
+					resource.TestCheckTypeSetElemAttr(path, "requirements.*.controls.*.rules_id.*", "def-000-be9"),
+					resource.TestCheckTypeSetElemAttr(path, "requirements.*.controls.*.rules_id.*", "def-000-cea"),
 				),
 			},
 		},
 	})
+}
+
+func TestCustomFramework_delete(t *testing.T) {
+	t.Parallel()
+	handle := fmt.Sprintf("handle-%d", rand.Intn(100000))
+	version := fmt.Sprintf("version-%d", rand.Intn(100000))
+
+	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
+	path := "datadog_custom_framework.sample_rules"
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: accProviders,
+		CheckDestroy:             testAccCheckDatadogFrameworkDestroy(ctx, providers.frameworkProvider, path, version, handle),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckDatadogCreateFramework(version, handle),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(path, "handle", handle),
+					resource.TestCheckResourceAttr(path, "version", version),
+				),
+			},
+			{
+				ResourceName:      path,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestCustomFramework_updateMultipleRequirements(t *testing.T) {
+	t.Parallel()
+	handle := fmt.Sprintf("handle-%d", rand.Intn(100000))
+	version := fmt.Sprintf("version-%d", rand.Intn(100000))
+
+	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
+	path := "datadog_custom_framework.sample_rules"
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: accProviders,
+		CheckDestroy:             testAccCheckDatadogFrameworkDestroy(ctx, providers.frameworkProvider, path, version, handle),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckDatadogCreateFrameworkWithMultipleRequirements(version, handle),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(path, "handle", handle),
+					resource.TestCheckResourceAttr(path, "version", version),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*", map[string]string{
+						"name": "compliance-requirement",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*", map[string]string{
+						"name": "security-requirement",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*.controls.*", map[string]string{
+						"name": "compliance-control",
+					}),
+					resource.TestCheckTypeSetElemNestedAttrs(path, "requirements.*.controls.*", map[string]string{
+						"name": "security-control",
+					}),
+					resource.TestCheckTypeSetElemAttr(path, "requirements.*.controls.*.rules_id.*", "def-000-be9"),
+					resource.TestCheckTypeSetElemAttr(path, "requirements.*.controls.*.rules_id.*", "def-000-cea"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDatadogCreateFrameworkWithMultipleRequirements(version string, handle string) string {
+	return fmt.Sprintf(`
+		resource "datadog_custom_framework" "sample_rules" {
+			version       = "%s"
+			handle        = "%s"
+			name          = "new-name"
+			description   = "test description"
+			icon_url      = "test url"
+			requirements  {
+				name = "security-requirement"
+				controls {
+					name = "security-control"
+					rules_id = ["def-000-cea", "def-000-be9"]
+				}
+			}
+			requirements  {
+				name = "compliance-requirement"
+				controls {
+					name = "compliance-control"
+					rules_id = ["def-000-be9", "def-000-cea"]
+				}
+			}
+		}
+	`, version, handle)
 }
 
 func testAccCheckDatadogCreateFramework(version string, handle string) string {
