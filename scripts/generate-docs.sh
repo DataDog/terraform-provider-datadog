8a49998e543a0211455992dd210392cd96fd88bc
Neha Konjeti <neha.konjeti@datadoghq.com>
2025-05-07 10:17:46 -0400

    fix docs

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -4,6 +4,7 @@
 # Add here the files to be excluded from the doc generation
 exclude_files=(
   "docs/resources/integration_aws_account.md"
+  "docs/resources/compliance_custom_framework.md"
 )
 
 # Check if manual changes were made to any excluded files and exit
