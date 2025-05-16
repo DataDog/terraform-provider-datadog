5376a3c017f509fcff7208ed2edf9db95f7d65c3
Neha Konjeti <neha.konjeti@datadoghq.com>
2025-05-16 12:38:25 -0400

    use real rule ids in the example resource

--- examples/resources/datadog_compliance_custom_framework/resource.tf
+++ examples/resources/datadog_compliance_custom_framework/resource.tf
@@ -7,18 +7,18 @@ resource "datadog_compliance_custom_framework" "example" {
     name = "requirement1"
     controls {
       name     = "control1"
-      rules_id = ["aaa-000-ccc", "bbb-000-ddd"]
+      rules_id = ["04w-clb-3io", "0eg-j5g-xip"]
     }
     controls {
       name     = "control2"
-      rules_id = ["aaa-000-lll"]
+      rules_id = ["def-000-11s"]
     }
   }
   requirements {
     name = "requirement2"
     controls {
       name     = "control3"
-      rules_id = ["aaa-000-zzz"]
+      rules_id = [""def-000-1im"]
     }
   }
 }
