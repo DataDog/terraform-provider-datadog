838b5e5e39ae2fb4d386ae9914d0192506104ef6
Belhorma Bendebiche <belhorma.bendebiche@datadoghq.com>
2025-05-23 14:18:11 +0200

    add On-Call resources
    Co-authored-by: Daniel Marin <daniel.marin@datadoghq.com>

---
+++ datadog/tests/resource_datadog_on_call_escalation_policy_test.tf
@@ -0,0 +1,50 @@
+
+resource "datadog_user" "policy_test" {
+  email = "USER_EMAIL"
+}
+
+resource "datadog_team" "policy_test" {
+  description = "Description"
+  handle      = "TEAM_HANDLE"
+  name        = "TEAM_NAME"
+}
+
+resource "datadog_on_call_schedule" "schedule" {
+  name      = "Escalation Policy Test Schedule UNIQ"
+  time_zone = "America/New_York"
+  teams     = [datadog_team.policy_test.id]
+  layer {
+    effective_date = "2025-01-01T00:00:00Z"
+    interval {
+      days    = 1
+      seconds = 300
+    }
+    rotation_start = "2025-01-01T00:00:00Z"
+    users          = [datadog_user.policy_test.id, null]
+    name           = "Primary On-Call Layer"
+    restriction {
+      end_day    = "monday"
+      end_time   = "17:00:00"
+      start_day  = "monday"
+      start_time = "09:00:00"
+    }
+  }
+}
+
+resource "datadog_on_call_escalation_policy" "policy_test" {
+  name                        = "POLICY_NAME"
+  resolve_page_on_policy_end = true
+  step {
+    assignment = "round-robin"
+    escalate_after_seconds = 300
+    target {
+      team = datadog_team.policy_test.id
+    }
+    target {
+      user = datadog_user.policy_test.id
+    }
+    target {
+      schedule = datadog_on_call_schedule.schedule.id
+    }
+  }
+}
