6ed071a59232ecd97976fa624a0e81b8aa068024
Mike Ball <mikedball@gmail.com>
2025-07-04 14:12:48 -0400

    feat: add `datadog_metric_metadata` data source
    This addresses issue #3099 and adds a `datadog_metric_metadata` data source,
    similar to the `datadog_metric_tags` data source.
    
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- datadog/fwprovider/framework_provider.go
+++ datadog/fwprovider/framework_provider.go
@@ -103,6 +103,7 @@ var Datasources = []func() datasource.DataSource{
 	NewDatadogApmRetentionFiltersOrderDataSource,
 	NewDatadogDashboardListDataSource,
 	NewDatadogIntegrationAWSNamespaceRulesDatasource,
+	NewDatadogMetricMetadataDataSource,
 	NewDatadogMetricTagsDataSource,
 	NewDatadogPowerpackDataSource,
 	NewDatadogServiceAccountDatasource,
