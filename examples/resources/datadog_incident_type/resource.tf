a1f2518e733bb5aad57c3a91f2fb2695836d2a31
cody.hahn <cody.hahn@datadoghq.com>
2025-08-04 13:33:33 -0400

    Update examples

--- examples/resources/datadog_incident_type/resource.tf
+++ examples/resources/datadog_incident_type/resource.tf
@@ -4,22 +4,3 @@ resource "datadog_incident_type" "example" {
   description = "Security-related incidents requiring immediate attention"
   is_default  = false
 }
-
-# Minimal configuration (description is optional)
-resource "datadog_incident_type" "minimal" {
-  name = "Simple Incident"
-}
-
-# Default incident type (only one can be default)
-resource "datadog_incident_type" "default" {
-  name        = "General Incident"
-  description = "Default incident type for unspecified incidents"
-  is_default  = true
-}
-
-# Incident type with longer description
-resource "datadog_incident_type" "detailed" {
-  name        = "Application Performance"
-  description = "Performance-related incidents affecting application response times, throughput, or availability that require immediate investigation and resolution"
-  is_default  = false
-}
