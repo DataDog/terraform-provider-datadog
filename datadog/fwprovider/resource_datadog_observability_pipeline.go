40f194d9e69227f7fb7a4385b51bec19bd75e966
Robert Blafford <robert.blafford@datadoghq.com>
2025-08-05 16:18:12 -0400

    [datadog_observability_piplines] Amazon Security Lake Destination

--- datadog/fwprovider/resource_datadog_observability_pipeline.go
+++ datadog/fwprovider/resource_datadog_observability_pipeline.go
@@ -303,6 +303,7 @@ type destinationsModel struct {
 	AmazonOpenSearchDestination   []*amazonOpenSearchDestinationModel                `tfsdk:"amazon_opensearch"`
 	SocketDestination             []*observability_pipeline.SocketDestinationModel   `tfsdk:"socket"`
 	AmazonS3Destination           []*observability_pipeline.AmazonS3DestinationModel `tfsdk:"amazon_s3"`
+	AmazonSecurityLakeDestination []*amazonSecurityLakeDestinationModel              `tfsdk:"amazon_security_lake"`
 }
 
 type amazonOpenSearchDestinationModel struct {
@@ -610,6 +611,16 @@ type googlePubSubSourceModel struct {
 
 type gcpAuthModel struct {
 	CredentialsFile types.String `tfsdk:"credentials_file"`
+}
+
+type amazonSecurityLakeDestinationModel struct {
+	Id               types.String                         `tfsdk:"id"`
+	Inputs           types.List                           `tfsdk:"inputs"`
+	Bucket           types.String                         `tfsdk:"bucket"`
+	Region           types.String                         `tfsdk:"region"`
+	CustomSourceName types.String                         `tfsdk:"custom_source_name"`
+	Tls              *tlsModel                            `tfsdk:"tls"`
+	Auth             *observability_pipeline.AwsAuthModel `tfsdk:"auth"`
 }
 
 func NewObservabilitPipelineResource() resource.Resource {
@@ -2253,6 +2264,38 @@ func (r *observabilityPipelineResource) Schema(_ context.Context, _ resource.Sch
 							},
 							"socket":    observability_pipeline.SocketDestinationSchema(),
 							"amazon_s3": observability_pipeline.AmazonS3DestinationSchema(),
+							"amazon_security_lake": schema.ListNestedBlock{
+								Description: "The `amazon_security_lake` destination sends your logs to Amazon Security Lake.",
+								NestedObject: schema.NestedBlockObject{
+									Attributes: map[string]schema.Attribute{
+										"id": schema.StringAttribute{
+											Required:    true,
+											Description: "Unique identifier for the destination component.",
+										},
+										"inputs": schema.ListAttribute{
+											ElementType: types.StringType,
+											Required:    true,
+											Description: "A list of component IDs whose output is used as the `input` for this component.",
+										},
+										"bucket": schema.StringAttribute{
+											Required:    true,
+											Description: "Name of the Amazon S3 bucket in Security Lake (3-63 characters).",
+										},
+										"region": schema.StringAttribute{
+											Required:    true,
+											Description: "AWS region of the Security Lake bucket.",
+										},
+										"custom_source_name": schema.StringAttribute{
+											Required:    true,
+											Description: "Custom source name for the logs in Security Lake.",
+										},
+									},
+									Blocks: map[string]schema.Block{
+										"tls":  tlsSchema(),
+										"auth": observability_pipeline.AwsAuthSchema(),
+									},
+								},
+							},
 						},
 					},
 				},
@@ -2557,6 +2600,9 @@ func expandPipeline(ctx context.Context, state *observabilityPipelineModel) (*da
 	for _, d := range state.Config.Destinations.AmazonS3Destination {
 		config.Destinations = append(config.Destinations, observability_pipeline.ExpandAmazonS3Destination(ctx, d))
 	}
+	for _, d := range state.Config.Destinations.AmazonSecurityLakeDestination {
+		config.Destinations = append(config.Destinations, expandObservabilityPipelinesAmazonSecurityLakeDestination(ctx, d))
+	}
 
 	attrs.SetConfig(*config)
 	data.SetAttributes(*attrs)
@@ -2744,6 +2790,9 @@ func flattenPipeline(ctx context.Context, state *observabilityPipelineModel, res
 		}
 		if d := observability_pipeline.FlattenAmazonS3Destination(ctx, d.ObservabilityPipelineAmazonS3Destination); d != nil {
 			outCfg.Destinations.AmazonS3Destination = append(outCfg.Destinations.AmazonS3Destination, d)
+		}
+		if d := flattenObservabilityPipelinesAmazonSecurityLakeDestination(ctx, d.ObservabilityPipelineAmazonSecurityLakeDestination); d != nil {
+			outCfg.Destinations.AmazonSecurityLakeDestination = append(outCfg.Destinations.AmazonSecurityLakeDestination, d)
 		}
 
 	}
@@ -4984,4 +5033,60 @@ func expandDatadogTagsProcessor(ctx context.Context, src *observability_pipeline
 
 func flattenDatadogTagsProcessor(ctx context.Context, src *datadogV2.ObservabilityPipelineDatadogTagsProcessor) *observability_pipeline.DatadogTagsProcessorModel {
 	return observability_pipeline.FlattenDatadogTagsProcessor(ctx, src)
+}
+
+func expandObservabilityPipelinesAmazonSecurityLakeDestination(ctx context.Context, src *amazonSecurityLakeDestinationModel) datadogV2.ObservabilityPipelineConfigDestinationItem {
+	dest := datadogV2.NewObservabilityPipelineAmazonSecurityLakeDestinationWithDefaults()
+	dest.SetId(src.Id.ValueString())
+
+	var inputs []string
+	src.Inputs.ElementsAs(ctx, &inputs, false)
+	dest.SetInputs(inputs)
+
+	if !src.Bucket.IsNull() {
+		dest.SetBucket(src.Bucket.ValueString())
+	}
+	if !src.Region.IsNull() {
+		dest.SetRegion(src.Region.ValueString())
+	}
+	if !src.CustomSourceName.IsNull() {
+		dest.SetCustomSourceName(src.CustomSourceName.ValueString())
+	}
+	if src.Tls != nil {
+		dest.Tls = expandTls(src.Tls)
+	}
+	if src.Auth != nil {
+		dest.Auth = observability_pipeline.ExpandAwsAuth(src.Auth)
+	}
+
+	return datadogV2.ObservabilityPipelineConfigDestinationItem{
+		ObservabilityPipelineAmazonSecurityLakeDestination: dest,
+	}
+}
+
+func flattenObservabilityPipelinesAmazonSecurityLakeDestination(ctx context.Context, src *datadogV2.ObservabilityPipelineAmazonSecurityLakeDestination) *amazonSecurityLakeDestinationModel {
+	if src == nil {
+		return nil
+	}
+
+	inputs, _ := types.ListValueFrom(ctx, types.StringType, src.GetInputs())
+
+	model := &amazonSecurityLakeDestinationModel{
+		Id:               types.StringValue(src.GetId()),
+		Inputs:           inputs,
+		Bucket:           types.StringValue(src.GetBucket()),
+		Region:           types.StringValue(src.GetRegion()),
+		CustomSourceName: types.StringValue(src.GetCustomSourceName()),
+	}
+
+	if src.Tls != nil {
+		tls := flattenTls(src.Tls)
+		model.Tls = &tls
+	}
+	if src.Auth != nil {
+		auth := observability_pipeline.FlattenAwsAuth(src.Auth)
+		model.Auth = auth
+	}
+
+	return model
 }
