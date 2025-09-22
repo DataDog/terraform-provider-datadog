33de28d737c22698751077241fa274a77c18ad39
Giorgos Dimitriou <giwrgosdi@gmail.com>
2025-09-22 16:43:43 +0300

    [datadog_application_key] Add preview Actions API access feature

--- examples/resources/datadog_application_key/resource.tf
+++ examples/resources/datadog_application_key/resource.tf
@@ -2,3 +2,9 @@
 resource "datadog_application_key" "foo" {
   name = "foo-application"
 }
+
+# Create a new Datadog Application Key with Actions API access (Preview)
+resource "datadog_application_key" "actions_enabled" {
+  name                      = "foo-application-with-actions"
+  enable_actions_api_access = true
+}
