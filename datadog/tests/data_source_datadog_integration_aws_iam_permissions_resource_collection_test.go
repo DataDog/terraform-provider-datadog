bbcea0eec034f5ca0ae618c104f687f8ea3ad5bd
Raymond Eah <raymond.eah@datadoghq.com>
2025-10-06 10:18:41 -0400

    write tests

---
+++ datadog/tests/data_source_datadog_integration_aws_iam_permissions_resource_collection_test.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/terraform-providers/terraform-provider-datadog/datadog/fwprovider"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+)
+
+func TestAccDatadogIntegrationAWSIAMPermissionsResourceCollectionDatasource(t *testing.T) {
+	_, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV5ProviderFactories: accProviders,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceIntegrationAWSIAMPermissionsResourceCollectionConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					checkDatadogIntegrationAWSIAMPermissionsResourceCollectionCount(providers.frameworkProvider),
+				),
+			},
+		},
+	})
+}
+
+func testAccDatasourceIntegrationAWSIAMPermissionsResourceCollectionConfig() string {
+	return `data "datadog_integration_aws_iam_permissions_ResourceCollection" "foo" {}`
+}
+
+func checkDatadogIntegrationAWSIAMPermissionsResourceCollectionCount(accProvider *fwprovider.FrameworkProvider) func(state *terraform.State) error {
+	return func(state *terraform.State) error {
+		apiInstances := accProvider.DatadogApiInstances
+		auth := accProvider.Auth
+
+		iamPermissions, _, err := apiInstances.GetAWSIntegrationApiV2().GetAWSIntegrationIAMPermissionsResourceCollection(auth)
+		if err != nil {
+			return err
+		}
+
+		resourceAttributes := state.RootModule().Resources["data.datadog_integration_aws_iam_permissions_ResourceCollection.foo"].Primary.Attributes
+		iamPermissionsCount, _ := strconv.Atoi(resourceAttributes["iam_permissions.#"])
+		permissionsDd := iamPermissions.Data.Attributes.Permissions
+
+		if iamPermissionsCount != len(permissionsDd) {
+			return fmt.Errorf("expected %d iam permissions, got %d iam permissions",
+				iamPermissionsCount, len(permissionsDd))
+		}
+
+		return nil
+	}
+}
