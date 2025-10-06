c7815b5b5c90321b90298f477d554fc56abd0b76
congx4 <cong.xie@datadoghq.com>
2025-10-06 16:14:16 -0400

    [datadog_tag_pipeline_ruleset] terraform support for tag-pipeline (#3236)
    add data_source and resource and doc for tag-pipeline
    
    use local go client
    
    add tf tests:
    
    record vcr
    
    add the reorder resources for tag pipeline
    
    record cassette
    
    update docs
    
    Merge branch 'master' of github.com:DataDog/terraform-provider-datadog into congxie/addTFTagPipeline
    
    remove local go client
    
    update docs
    
    fix the test
    
    vcr the tests
    
    fix docs
    
    fix tests
    
    fix the tests
    
    fix tests
    
    fix fmt
    
    [Fix] Fixing a plan error caused by CLI version < 1.3.8
    
    An old Terraform CLI bug (issue#32460) is the cause of some tests failing.
    Creating a ModifyPlan() and setting some blocks explicitly to nil should
    fix the issue.
    
    remove computed field
    
    Merge branch 'congxie/addTFTagPipeline' of github.com:DataDog/terraform-provider-datadog into congxie/addTFTagPipeline
    
    resolve comments
    
    update the tests
    
    removed old tests
    
    modify doc
    
    record rulesets vcr
    
    record ruleset vcr
    
    resolve comments
    
    resolve conflicts
    
    fix tests
    
    Co-authored-by: Supam <thibault.viennot@datadoghq.com>

---
+++ datadog/tests/data_source_datadog_tag_pipeline_ruleset_test.go
@@ -0,0 +1,185 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccDatadogTagPipelineRulesetDataSource_Basic(t *testing.T) {
+	t.Parallel()
+	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
+	rulesetName := uniqueEntityName(ctx, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: accProviders,
+		CheckDestroy:             testAccCheckDatadogTagPipelineRulesetDestroy(ctx, providers.frameworkProvider),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckDatadogTagPipelineRulesetDataSourceConfig(rulesetName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "name", rulesetName),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "enabled", "true"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.#", "1"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.0.enabled", "true"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.0.name", "Add Team Tag"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.0.query.query", "service:web-api"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.0.query.case_insensitivity", "true"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.0.query.if_not_exists", "true"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.0.query.addition.key", "team"),
+					resource.TestCheckResourceAttr(
+						"data.datadog_tag_pipeline_ruleset.foo", "rules.0.query.addition.value", "backend"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDatadogTagPipelineRulesetDataSource_MappingRule(t *testing.T) {
+	t.Parallel()
+	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
+	uniq := uniqueEntityName(ctx, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: accProviders,
+		CheckDestroy:             testAccCheckDatadogTagPipelineRulesetDestroy(ctx, providers.frameworkProvider),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceTagPipelineRulesetMappingConfig(uniq),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "name", fmt.Sprintf("tf-test-mapping-ruleset-%s", uniq)),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.mapping.destination_key", "env"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.mapping.if_not_exists", "true"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.mapping.source_keys.#", "2"),
+					resource.TestCheckTypeSetElemAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.mapping.source_keys.*", "environment"),
+					resource.TestCheckTypeSetElemAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.mapping.source_keys.*", "stage"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDatadogTagPipelineRulesetDataSource_ReferenceTableRule(t *testing.T) {
+	t.Parallel()
+	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
+	uniq := uniqueEntityName(ctx, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV5ProviderFactories: accProviders,
+		CheckDestroy:             testAccCheckDatadogTagPipelineRulesetDestroy(ctx, providers.frameworkProvider),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatasourceTagPipelineRulesetReferenceTableConfig(uniq),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "name", fmt.Sprintf("tf-test-ref-table-ruleset-%s", uniq)),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.table_name", "service_mapping"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.case_insensitivity", "true"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.if_not_exists", "false"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.source_keys.#", "1"),
+					resource.TestCheckTypeSetElemAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.source_keys.*", "service"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.field_pairs.#", "2"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.field_pairs.0.input_column", "service_name"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.field_pairs.0.output_key", "service"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.field_pairs.1.input_column", "team_name"),
+					resource.TestCheckResourceAttr("data.datadog_tag_pipeline_ruleset.foo", "rules.0.reference_table.field_pairs.1.output_key", "team"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDatadogTagPipelineRulesetDataSourceConfig(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_tag_pipeline_ruleset" "foo" {
+  name = "%s"
+  enabled = true
+  rules {
+    enabled = true
+    name = "Add Team Tag"
+    query {
+      query = "service:web-api"
+      case_insensitivity = true
+      if_not_exists = true
+      addition {
+        key = "team"
+        value = "backend"
+      }
+    }
+  }
+}
+
+data "datadog_tag_pipeline_ruleset" "foo" {
+  id = datadog_tag_pipeline_ruleset.foo.id
+}`, uniq)
+}
+
+func testAccDatasourceTagPipelineRulesetMappingConfig(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_tag_pipeline_ruleset" "foo" {
+  name    = "tf-test-mapping-ruleset-%s"
+  enabled = true
+
+  rules {
+    name    = "mapping-rule"
+    enabled = true
+    
+    mapping {
+      destination_key = "env"
+      if_not_exists   = true
+      source_keys     = ["environment", "stage"]
+    }
+  }
+}
+
+data "datadog_tag_pipeline_ruleset" "foo" {
+  id = datadog_tag_pipeline_ruleset.foo.id
+}`, uniq)
+}
+
+func testAccDatasourceTagPipelineRulesetReferenceTableConfig(uniq string) string {
+	return fmt.Sprintf(`
+resource "datadog_tag_pipeline_ruleset" "foo" {
+  name    = "tf-test-ref-table-ruleset-%s"
+  enabled = true
+
+  rules {
+    name    = "ref-table-rule"
+    enabled = true
+    
+    reference_table {
+      table_name         = "service_mapping"
+      case_insensitivity = true
+      if_not_exists      = false
+      source_keys        = ["service"]
+      
+      field_pairs {
+        input_column = "service_name"
+        output_key   = "service"
+      }
+      
+      field_pairs {
+        input_column = "team_name"
+        output_key   = "team"
+      }
+    }
+  }
+}
+
+data "datadog_tag_pipeline_ruleset" "foo" {
+  id = datadog_tag_pipeline_ruleset.foo.id
+}`, uniq)
+}
