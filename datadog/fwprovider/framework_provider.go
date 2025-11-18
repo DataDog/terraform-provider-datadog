9368331d45983d828397daad08de9444dbb5dcdb
Mike Ball <mikedball@gmail.com>
2025-11-18 04:44:56 -0500

    feat: add 'datadog_metrics' data source (#3214)
    This addresses issue #3213 by adding a `datadog_metrics` data source for
    interacting with the [search metrics API](https://docs.datadoghq.com/api/latest/metrics/#search-metrics).
    
    The data source offers a way to dynamically search & fetch metrics, then
    use the results to drive other Terraform configurations.
    
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- datadog/fwprovider/framework_provider.go
+++ datadog/fwprovider/framework_provider.go
@@ -123,6 +123,7 @@ var Datasources = []func() datasource.DataSource{
 	NewDatadogMetricActiveTagsAndAggregationsDataSource,
 	NewDatadogMetricMetadataDataSource,
 	NewDatadogMetricTagsDataSource,
+	NewDatadogMetricsDataSource,
 	NewDatadogPowerpackDataSource,
 	NewDatadogServiceAccountDatasource,
 	NewDatadogSoftwareCatalogDataSource,
