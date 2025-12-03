822b2c0e7ca1d95b200515fe8a495cda4b5fcd11
Deepak Shivaram <deepak.shivaram@datadoghq.com>
2025-12-03 14:42:50 -0500

    fmt

--- examples/resources/datadog_team-hierarchy-links/resource.tf
+++ examples/resources/datadog_team-hierarchy-links/resource.tf
@@ -1,6 +1,6 @@
 # Create new team-hierarchy-links resource
 
 resource "datadog_team-hierarchy-links" "foo" {
-    parent_team_id = "692e8073-12c4-4c71-8408-5090bd44c9c8"
-    sub_team_id    = "d10bf972-ca7c-4181-9668-6db783533f6e"
+ parent_team_id = "692e8073-12c4-4c71-8408-5090bd44c9c8"
+ sub_team_id    = "d10bf972-ca7c-4181-9668-6db783533f6e"
 }
