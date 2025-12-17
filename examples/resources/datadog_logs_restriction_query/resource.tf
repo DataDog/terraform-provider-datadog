0c299f221ec7b1fb70d51bc4001097e9eeeb406f
Dinu Wijetunga <dinu.wijetunga@datadoghq.com>
2025-12-17 12:17:47 -0500

    add example tf resource

---
+++ examples/resources/datadog_logs_restriction_query/resource.tf
@@ -0,0 +1,9 @@
+# Manage a Datadog log restriction query
+resource "datadog_logs_restriction_query" "test_lrq" {
+	restriction_query = "service:foo"
+
+    role_ids = [
+        "00000000-0000-1111-0000-000000000000",
+        "11111111-1111-0000-1111-111111111111",
+    ]
+}
