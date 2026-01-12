e48e58a7e1d9eaed2eb709451cf8054d2350a339
Andy Yaco-Mink <andy.yacomink@datadoghq.com>
2026-01-12 10:19:54 -0500

    more VCR's

--- datadog/tests/resource_datadog_dashboard_json_test.go
+++ datadog/tests/resource_datadog_dashboard_json_test.go
@@ -177,7 +177,7 @@ func TestAccDatadogDashboardJSONRbacDiff(t *testing.T) {
 				Config: testAccCheckDatadogDashboardJSONRbacDiff(uniqueName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
-						"datadog_dashboard_json.timeboard_json", "dashboard", fmt.Sprintf("{\"description\":\"Created using the Datadog provider in Terraform\",\"is_read_only\":false,\"layout_type\":\"ordered\",\"notify_list\":[],\"restricted_roles\":[],\"template_variables\":[],\"title\":\"%s\",\"widgets\":[{\"definition\":{\"alert_id\":\"895605\",\"precision\":3,\"text_align\":\"center\",\"title\":\"Widget Title\",\"type\":\"alert_value\",\"unit\":\"b\"}}]}", uniqueName)),
+						"datadog_dashboard_json.timeboard_json", "dashboard", fmt.Sprintf("{\"description\":\"Created using the Datadog provider in Terraform\",\"layout_type\":\"ordered\",\"notify_list\":[],\"restricted_roles\":[],\"template_variables\":[],\"title\":\"%s\",\"widgets\":[{\"definition\":{\"alert_id\":\"895605\",\"precision\":3,\"text_align\":\"center\",\"title\":\"Widget Title\",\"type\":\"alert_value\",\"unit\":\"b\"}}]}", uniqueName)),
 				),
 			},
 		},
@@ -221,7 +221,7 @@ func TestAccDatadogDashboardJSONNotifyListDiff(t *testing.T) {
 				Config: testAccCheckDatadogDashboardJSONNotifyListDiff(uniqueName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
-						"datadog_dashboard_json.timeboard_json", "dashboard", fmt.Sprintf("{\"description\":\"Created using the Datadog provider in Terraform\",\"is_read_only\":false,\"layout_type\":\"ordered\",\"notify_list\":[\"a-user@example.com\",\"k-user@example.com\",\"z-user1@example.com\"],\"restricted_roles\":[],\"template_variables\":[],\"title\":\"%s\",\"widgets\":[]}", uniqueName)),
+						"datadog_dashboard_json.timeboard_json", "dashboard", fmt.Sprintf("{\"description\":\"Created using the Datadog provider in Terraform\",\"layout_type\":\"ordered\",\"notify_list\":[\"a-user@example.com\",\"k-user@example.com\",\"z-user1@example.com\"],\"restricted_roles\":[],\"template_variables\":[],\"title\":\"%s\",\"widgets\":[]}", uniqueName)),
 				),
 			},
 		},
