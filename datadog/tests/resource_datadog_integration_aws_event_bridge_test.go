3bf3a98fc38fca63a0b30e7a31d76784af7bd031
Francesco Pighi <francesco.pighi@datadoghq.com>
2026-01-26 11:59:48 +0100

    Skip cloud integration tests in non-replay mode
    Tests require AWS/GCP integrations that aren't configured in the test org.

--- datadog/tests/resource_datadog_integration_aws_event_bridge_test.go
+++ datadog/tests/resource_datadog_integration_aws_event_bridge_test.go
@@ -13,6 +13,9 @@ import (
 )
 
 func TestAccIntegrationAwsEventBridgeBasic(t *testing.T) {
+	if !isReplaying() {
+		t.Skip("This test only supports replaying - requires AWS account integrated")
+	}
 	t.Parallel()
 	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
 	accountID := uniqueAWSAccountID(ctx, t)
