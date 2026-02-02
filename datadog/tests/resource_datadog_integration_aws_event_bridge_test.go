8b2b0568db837c3a953fd1d82bdf10805bd12b40
David Han <david.han@datadoghq.com>
2026-02-02 14:07:04 -0500

    Use random account with externalID for tests

--- datadog/tests/resource_datadog_integration_aws_event_bridge_test.go
+++ datadog/tests/resource_datadog_integration_aws_event_bridge_test.go
@@ -18,7 +18,7 @@ func TestAccIntegrationAwsEventBridgeBasic(t *testing.T) {
 	}
 	t.Parallel()
 	ctx, providers, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
-	accountID := "694513996623"
+	accountID := uniqueAWSAccountID(ctx, t)
 	uniq := uniqueEntityName(ctx, t)
 
 	resource.Test(t, resource.TestCase{
@@ -37,12 +37,35 @@ func TestAccIntegrationAwsEventBridgeBasic(t *testing.T) {
 
 func testAccCheckDatadogIntegrationAwsEventBridge(accountID string, uniq string) string {
 	return fmt.Sprintf(`
+	resource "datadog_integration_aws_account" "account" {
+		aws_account_id = "%s"
+		aws_partition = "aws"
+		aws_regions {}
+		auth_config {
+			aws_auth_config_role {
+				role_name = "DatadogIntegrationRole"
+				external_id = "8b424c40dafa4034ab825bd16ccc74dd"
+			}
+		}
+		logs_config {
+			lambda_forwarder {}
+		}
+		metrics_config {
+			namespace_filters {}
+		}
+		resources_config {}
+		traces_config {
+			xray_services {}
+		}
+	}
+
 	resource "datadog_integration_aws_event_bridge" "foo" {
 		account_id = "%s"
 		event_generator_name = "%s"
 		create_event_bus = false
 		region = "us-east-1"
-	}`, accountID, uniq)
+		depends_on = [datadog_integration_aws_account.account]
+	}`, accountID, accountID, uniq)
 }
 
 func testAccCheckDatadogIntegrationAwsEventBridgeDestroy(accProvider *fwprovider.FrameworkProvider) func(*terraform.State) error {
