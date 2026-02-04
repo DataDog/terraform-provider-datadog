ae0e060e7e7b31ff28fbb4d158d25acf77893824
Francesco Pighi <francesco.pighi@datadoghq.com>
2026-02-04 09:41:09 +0100

    Fix metric metadata datasource test by creating resource first
    The datasource test was trying to query a metric that didn't exist. Changed
    to a two-step test pattern: first create the metric resource and post an
    event for it, then wait 3 seconds for indexing, then query it via datasource.
    Added missing dependencies: time, terraform imports.

--- datadog/tests/data_source_datadog_metric_metadata_test.go
+++ datadog/tests/data_source_datadog_metric_metadata_test.go
@@ -3,41 +3,103 @@ package test
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/terraform-providers/terraform-provider-datadog/datadog"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/jonboulle/clockwork"
+	communityClient "github.com/zorkian/go-datadog-api"
 )
 
 func TestAccDatadogMetricMetadataDatasource(t *testing.T) {
 	t.Parallel()
-	_, _, accProviders := testAccFrameworkMuxProviders(context.Background(), t)
-	metric := "foo"
+	ctx, accProviders := testAccProviders(context.Background(), t)
+	accProvider := testAccProvider(t, accProviders)
+	_, _, fwAccProviders := testAccFrameworkMuxProviders(context.Background(), t)
+	metricName := strings.ReplaceAll(uniqueEntityName(ctx, t), "-", "_")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV5ProviderFactories: accProviders,
+		ProtoV5ProviderFactories: fwAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDatasourceMetricMetadataConfig(metric),
+				// Step 1: Create the metric resource first
+				Config: testAccDatasourceMetricMetadataResourceConfig(metricName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "metric_name", metric),
+					checkPostEventDatasource(accProvider, clockFromContext(ctx), metricName),
+					resource.TestCheckResourceAttr("datadog_metric_metadata.test", "metric", metricName),
+				),
+			},
+			{
+				// Step 2: Wait for indexing, then read with datasource
+				PreConfig: func() { time.Sleep(3 * time.Second) },
+				Config:    testAccDatasourceMetricMetadataFullConfig(metricName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "metric_name", metricName),
 					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "short_name", "short name for metric_metadata foo"),
 					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "description", "some description"),
 					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "unit", "byte"),
 					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "per_unit", "second"),
 					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "statsd_interval", "1"),
 					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "type", "gauge"),
-					resource.TestCheckResourceAttr("data.datadog_metric_metadata.foo", "integration", ""),
 				),
 			},
 		},
 	})
 }
 
-func testAccDatasourceMetricMetadataConfig(metric string) string {
-	return fmt.Sprintf(`
-data "datadog_metric_metadata" "foo" {
-	metric_name = "%s"
+func checkPostEventDatasource(accProvider func() (*schema.Provider, error), clock clockwork.FakeClock, metricName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		provider, _ := accProvider()
+		providerConf := provider.Meta().(*datadog.ProviderConfiguration)
+		client := providerConf.CommunityClient
+		datapointUnixTime := float64(clock.Now().Unix())
+		datapointValue := float64(1)
+		metric := communityClient.Metric{
+			Metric: communityClient.String(metricName),
+			Points: []communityClient.DataPoint{{&datapointUnixTime, &datapointValue}},
+		}
+		if err := client.PostMetrics([]communityClient.Metric{metric}); err != nil {
+			return err
+		}
+		return nil
+	}
 }
-`, metric)
+
+func testAccDatasourceMetricMetadataResourceConfig(metricName string) string {
+	return fmt.Sprintf(`
+resource "datadog_metric_metadata" "test" {
+  metric = "%s"
+  short_name = "short name for metric_metadata foo"
+  type = "gauge"
+  description = "some description"
+  unit = "byte"
+  per_unit = "second"
+  statsd_interval = "1"
+}
+`, metricName)
+}
+
+func testAccDatasourceMetricMetadataFullConfig(metricName string) string {
+	return fmt.Sprintf(`
+resource "datadog_metric_metadata" "test" {
+  metric = "%s"
+  short_name = "short name for metric_metadata foo"
+  type = "gauge"
+  description = "some description"
+  unit = "byte"
+  per_unit = "second"
+  statsd_interval = "1"
+}
+
+data "datadog_metric_metadata" "foo" {
+  metric_name = "%s"
+  depends_on = [datadog_metric_metadata.test]
+}
+`, metricName, metricName)
 }
