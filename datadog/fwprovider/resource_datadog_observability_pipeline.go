66fd6f2d60487034bda4d1aa752e492b4a96aa96
Armand Thibaudon <58812873+armleth@users.noreply.github.com>
2026-04-03 17:41:02 +0200

    [datadog_observability_pipeline] Make datadog agent source address configurable (#3613)
    feat: add datadog agent address_key field
    
    docs: regenerate docs
    
    feat: new/updated cassettes addition
    
    feat: new/updated cassettes addition, part 2
    
    Merge branch 'master' into armleth/obs_pipelines_make_datadog_agent_address_configurable
    
    fix: fixing wrong merge conflict resolve
    
    feat: setting the right go client
    
    Merge branch 'master' into armleth/obs_pipelines_make_datadog_agent_address_configurable
    
    feat: last version of go-client
    
    Merge branch 'master' into armleth/obs_pipelines_make_datadog_agent_address_configurable
    
    feat: cassettes
    
    feat: good go.mod/sum
    
    Merge branch 'master' into armleth/obs_pipelines_make_datadog_agent_address_configurable
    
    feat: cassettes and go.*
    
    feat: trying another go.sum and mod as main go clien is broken
    
    feat: cassettes
    
    Merge branch 'master' into armleth/obs_pipelines_make_datadog_agent_address_configurable
    
    feat: trying to reset cassettes, interactive rebase will follow
    
    feat: new cassettes (part 39430843948)
    
    Co-authored-by: armand.thibaudon <armand.thibaudon@datadoghq.com>

--- datadog/fwprovider/resource_datadog_observability_pipeline.go
+++ datadog/fwprovider/resource_datadog_observability_pipeline.go
@@ -128,7 +128,8 @@ type logstashSourceModel struct {
 }
 
 type datadogAgentSourceModel struct {
-	Tls []observability_pipeline.TlsModel `tfsdk:"tls"`
+	AddressKey types.String                      `tfsdk:"address_key"`
+	Tls        []observability_pipeline.TlsModel `tfsdk:"tls"`
 }
 
 type kafkaSourceModel struct {
@@ -789,6 +790,12 @@ func (r *observabilityPipelineResource) Schema(_ context.Context, _ resource.Sch
 									"datadog_agent": schema.ListNestedBlock{
 										Description: "The `datadog_agent` source collects logs from the Datadog Agent.",
 										NestedObject: schema.NestedBlockObject{
+											Attributes: map[string]schema.Attribute{
+												"address_key": schema.StringAttribute{
+													Optional:    true,
+													Description: "Name of the environment variable or secret that holds the listen address for the Datadog Agent source.",
+												},
+											},
 											Blocks: map[string]schema.Block{
 												"tls": observability_pipeline.TlsSchema(),
 											},
@@ -3219,6 +3226,9 @@ func flattenDatadogAgentSource(src *datadogV2.ObservabilityPipelineDatadogAgentS
 		return nil
 	}
 	out := &datadogAgentSourceModel{}
+	if v, ok := src.GetAddressKeyOk(); ok {
+		out.AddressKey = types.StringValue(*v)
+	}
 	if src.Tls != nil {
 		out.Tls = observability_pipeline.FlattenTls(src.Tls)
 	}
@@ -3228,7 +3238,11 @@ func flattenDatadogAgentSource(src *datadogV2.ObservabilityPipelineDatadogAgentS
 func expandDatadogAgentSource(src *datadogAgentSourceModel, id string) datadogV2.ObservabilityPipelineConfigSourceItem {
 	agent := datadogV2.NewObservabilityPipelineDatadogAgentSourceWithDefaults()
 	agent.SetId(id)
+	if !src.AddressKey.IsNull() {
+		agent.SetAddressKey(src.AddressKey.ValueString())
+	}
 	agent.Tls = observability_pipeline.ExpandTls(src.Tls)
+
 	return datadogV2.ObservabilityPipelineConfigSourceItem{
 		ObservabilityPipelineDatadogAgentSource: agent,
 	}
