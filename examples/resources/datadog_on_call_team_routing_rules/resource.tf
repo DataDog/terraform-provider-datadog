89b64d8e43b8e7c2d2537542e25707d58b1b2686
alychoi-dd <alyssa.choi@datadoghq.com>
2026-06-01 11:26:53 -0400

    [datadog_on_call_team_routing_rules] Add support for On-Call escalation policy actions (#3788)
    add escalation policy action type
    
    modified
    
    Merge branch 'master' of github.com:DataDog/terraform-provider-datadog into alychoi/add-ack-timeout
    
    modified
    
    fix rule placement
    
    resolve merge conflicts
    
    modify stateFromResponse
    
    remove unnecessary check
    
    modify example and generate doc
    
    modified stateFromResponse
    
    added validateconfig
    
    changed wording
    
    update codeowners
    
    Co-authored-by: Bnjis <benjamin.lienart@datadoghq.com>

--- examples/resources/datadog_on_call_team_routing_rules/resource.tf
+++ examples/resources/datadog_on_call_team_routing_rules/resource.tf
@@ -23,4 +23,48 @@ resource "datadog_on_call_team_routing_rules" "team_rules_test" {
     escalation_policy = "00000000-aba2-0000-0000-000000000000"
     urgency           = "dynamic"
   }
+
+  rule {
+    query = "tags.service:payment"
+    action {
+      escalation_policy {
+        policy_id           = "00000000-aba2-0000-0000-000000000000"
+        ack_timeout_minutes = 30
+        urgency             = "low"
+        support_hours {
+          time_zone = "America/New_York"
+          restriction {
+            start_day  = "monday"
+            start_time = "09:00:00"
+            end_day    = "monday"
+            end_time   = "17:00:00"
+          }
+          restriction {
+            start_day  = "tuesday"
+            start_time = "09:00:00"
+            end_day    = "tuesday"
+            end_time   = "17:00:00"
+          }
+          restriction {
+            start_day  = "wednesday"
+            start_time = "09:00:00"
+            end_day    = "wednesday"
+            end_time   = "17:00:00"
+          }
+          restriction {
+            start_day  = "thursday"
+            start_time = "09:00:00"
+            end_day    = "thursday"
+            end_time   = "17:00:00"
+          }
+          restriction {
+            start_day  = "friday"
+            start_time = "09:00:00"
+            end_day    = "friday"
+            end_time   = "17:00:00"
+          }
+        }
+      }
+    }
+  }
 }
