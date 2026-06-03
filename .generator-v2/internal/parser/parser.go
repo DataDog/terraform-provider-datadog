1af5e630f313628d3704da7923774a169ec90b48
Thibault Viennot <Supam@users.noreply.github.com>
2026-06-03 14:26:36 +0200

    [TFgen] Added a identifier translator to match the go SDK (#3850)
    * [TFgen] Added a identifier translator to match the goSDK
    
    The translator will transform the OAS snake_case names into the
    equivalent PascalCase that are used in the go SDK.
    
    Removed internal ticket names.
    
    * [TFgen] Added .gitignore

--- .generator-v2/internal/parser/parser.go
+++ .generator-v2/internal/parser/parser.go
@@ -3,7 +3,6 @@ package parser
 import "github.com/pb33f/libopenapi"
 
 // LoadSpec loads and parses the OpenAPI specification at path.
-// Full implementation in T009.
 func LoadSpec(path string) (libopenapi.Document, error) {
 	panic("not implemented")
 }
