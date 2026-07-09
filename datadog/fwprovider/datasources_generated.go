21dad27cacd2bb2c214e64880771e804167986cf
j10czar <jason.tenczar@datadoghq.com>
2026-07-09 16:47:41 -0400

    [authn-mappings] Add datadog_authn_mapping data source (generated)

--- datadog/fwprovider/datasources_generated.go
+++ datadog/fwprovider/datasources_generated.go
@@ -9,4 +9,6 @@ import "github.com/hashicorp/terraform-plugin-framework/datasource"
 //
 // FrameworkProvider.DataSources registers this slice alongside the hand-written
 // Datasources.
-var generatedDatasources = []func() datasource.DataSource{}
+var generatedDatasources = []func() datasource.DataSource{
+	NewDatadogAuthnMappingDataSource,
+}
