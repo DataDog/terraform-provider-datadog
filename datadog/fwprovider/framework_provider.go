1c762f50fd363e801125cbf43f9a75daecb9dcf5
Nick Venenga <nick@venenga.com>
2026-08-06 18:12:28 -0400

    [datadog_apm_services] Add APM services data source
    Adds a datadog_apm_services data source backed by the APM "Get service
    list" endpoint (GET /api/v2/apm/services), returning the traced/USM
    service names for a given environment filter.

--- datadog/fwprovider/framework_provider.go
+++ datadog/fwprovider/framework_provider.go
@@ -156,6 +156,7 @@ var Datasources = []func() datasource.DataSource{
 	NewAwsIntegrationIAMPermissionsResourceCollectionDataSource,
 	NewAwsLogsServicesDataSource,
 	NewDatadogApmRetentionFiltersOrderDataSource,
+	NewDatadogApmServicesDataSource,
 	NewDatadogDashboardListDataSource,
 	NewDatadogIntegrationAWSNamespaceRulesDatasource,
 	NewDatadogMetricActiveTagsAndAggregationsDataSource,
