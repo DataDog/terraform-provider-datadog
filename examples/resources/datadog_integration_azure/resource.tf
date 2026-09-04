82bea3f8bf52625f5a475b8e1d988ecfbb5c0319
kendle.lam <kendle.lam@datadoghq.com>
2026-09-04 20:18:27 +0000

    [datadog_integration_azure] Add app registration display name

--- examples/resources/datadog_integration_azure/resource.tf
+++ examples/resources/datadog_integration_azure/resource.tf
@@ -3,6 +3,7 @@ resource "datadog_integration_azure" "sandbox" {
   tenant_name              = "<azure_tenant_name>"
   client_id                = "<azure_client_id>"
   client_secret            = "<azure_client_secret_key>"
+  display_name             = "datadog-azure-integration"
   host_filters             = "examplefilter:true,example:true"
   app_service_plan_filters = "examplefilter:true,example:another"
   container_app_filters    = "examplefilter:true,example:one_more"
